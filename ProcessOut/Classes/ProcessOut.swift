8c5a0e44c9e99d8431ac748cff645bf78271ff64
Mauro Vime Castillo <mvimeapps@gmail.com>
2018-11-20 13:22:33 +0100

    Styling

--- ProcessOut/Classes/ProcessOut.swift
+++ ProcessOut/Classes/ProcessOut.swift
@@ -4,7 +4,6 @@ import Foundation
 
 public class ProcessOut {
     
-    
     public enum ProcessOutException: Error {
         case NetworkError
         case MissingProjectId
@@ -31,25 +30,24 @@ public class ProcessOut {
     private static var ApiUrl: String = "https://api.processout.com"
     private static var ProjectId: String?
     
-    
     public static func Setup(projectId: String) {
         ProcessOut.ProjectId = projectId
     }
     
     public static func Tokenize(card: Card, metadata: [String: Any]?, completion: @escaping (String?, ProcessOutException?) -> Void) {
         var parameters: [String: Any] = [:]
+        if let metadata = metadata {
+            parameters["metadata"] = metadata
+        }
+        parameters["name"] = card.Name
         parameters["number"] = card.CardNumber
-        GenerateToken(card: card, metadata: metadata, completion: {(tokenResponse, error) -> Void in
-            if error != nil || tokenResponse == nil {
-                completion(nil, error)
-            } else {
-                if let card = tokenResponse!["card"] as? [String: Any], let token = card["id"] as? String {
-                    completion(token, nil)
-                } else {
-                    completion(nil, error)
-                }
-            }
-        })
+        parameters["exp_month"] = card.ExpMonth
+        parameters["exp_year"] = card.ExpYear
+        if let cvc = card.CVC {
+            parameters["cvc2"] = cvc
+        }
+      
+        HttpRequest(route: "/cards", method: .post, parameters: parameters, completion: completion)
     }
     
     public static func UpdateCvc(cardId: String, newCvc: String, completion: @escaping (ProcessOutException?) -> Void) {
@@ -62,47 +60,30 @@ public class ProcessOut {
         }
     }
     
-    private static func GenerateToken(card: Card, metadata: [String: Any]?, completion: @escaping ([String: Any]?, ProcessOutException?) -> Void) {
-        var parameters: [String: Any] = [:]
-        if metadata != nil {
-            parameters["metadata"] = metadata!
-        }
-        parameters["name"] = card.Name
-        parameters["number"] = card.CardNumber
-        parameters["exp_month"] = card.ExpMonth
-        parameters["exp_year"] = card.ExpYear
-        if card.CVC != nil {
-            parameters["cvc2"] = card.CVC!
-        }
-    
-        
-        HttpRequest(route: "/cards", method: .post, parameters: parameters, completion: completion)
-
-    }
-    
     private static func HttpRequest(route: String, method: HTTPMethod, parameters: Parameters, completion: @escaping ([String: Any]?, ProcessOutException?) -> Void) {
-        var headers: HTTPHeaders = [:]
-        if let projectId = ProjectId, let authorizationHeader = Request.authorizationHeader(user: projectId, password: "") {
-            headers[authorizationHeader.key] = authorizationHeader.value
-            Alamofire.request(ApiUrl + route, method: method, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON(completionHandler: {(response) -> Void in
-                if let data = response.result.value as? [String: Any] {
-                    if let success = data["success"] as? Bool, success {
-                        completion(data, nil)
-                    } else {
-                        if let errorMessage = data["message"] as? String, let errorType = data["error_type"] as? String {
-                            completion(nil, ProcessOutException.BadRequest(errorMessage: errorMessage, errorCode: errorType))
-                        } else {
-                            
-                            completion(nil, ProcessOutException.InternalError)
-                        }
-                    }
-                } else {
-                    completion(nil, ProcessOutException.NetworkError)
-                }
-            })
-        } else {
+        guard let projectId = ProjectId, let authorizationHeader = Request.authorizationHeader(user: projectId, password: "") else {
             completion(nil, ProcessOutException.MissingProjectId)
+            return
         }
+      
+        var headers: HTTPHeaders = [:]
+      
+        headers[authorizationHeader.key] = authorizationHeader.value
+        Alamofire.request(ApiUrl + route, method: method, parameters: parameters, encoding: JSONEncoding.default, headers: headers).responseJSON(completionHandler: {(response) -> Void in
+            if let data = response.result.value as? [String: Any] {
+                if let success = data["success"] as? Bool, success {
+                    completion(data, nil)
+                } else {
+                    if let errorMessage = data["message"] as? String, let errorType = data["error_type"] as? String {
+                        completion(nil, ProcessOutException.BadRequest(errorMessage: errorMessage, errorCode: errorType))
+                    } else {
+                        completion(nil, ProcessOutException.InternalError)
+                    }
+                }
+            } else {
+                completion(nil, ProcessOutException.NetworkError)
+            }
+        })
     }
     
 }
