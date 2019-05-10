b060bf4a9a9d97db6db4162f59d32cd3cc461e77
Jeremy Lejoux <jeremy@processout.com>
2019-05-10 09:33:12 +0200

    alamofire: update alamofire

--- Example/ProcessOut/AppDelegate.swift
+++ Example/ProcessOut/AppDelegate.swift
@@ -15,7 +15,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         ProcessOut.Setup(projectId: "proj_dHvuowrjviYWm7ZX0hXlb7X2yaxdgo06")
         return true
