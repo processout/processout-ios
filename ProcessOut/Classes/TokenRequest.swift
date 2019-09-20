0c0937fdbdc6954a65c8e251bd404db0eccd9ac7
Jeremy Lejoux <jeremy@processout.com>
2019-09-20 17:26:44 +0200

    token: refactoring on token actions

--- ProcessOut/Classes/TokenRequest.swift
+++ ProcessOut/Classes/TokenRequest.swift
@@ -9,9 +9,11 @@ import Foundation
 
 class TokenRequest: Encodable {
     var source: String = ""
+    var verify: Bool = true
     
     enum CodingKeys: String, CodingKey {
         case source = "source"
+        case verify = "verify"
     }
     
     init(source: String) {
