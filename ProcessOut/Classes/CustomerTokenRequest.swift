1800be3096553e6cebd3e21e886be1ee82cffd51
Jeremy Lejoux <jeremy@processout.com>
2019-09-30 14:02:33 +0200

    token: add requests files

--- ProcessOut/Classes/CustomerTokenRequest.swift
+++ ProcessOut/Classes/CustomerTokenRequest.swift
@@ -9,9 +9,13 @@ import Foundation
 
 public class CustomerTokenRequest: Encodable {
     var source: String = ""
+    var verify: Bool = true
+    var threeDS2Enabled = true
     
     enum CodingKeys: String, CodingKey {
         case source = "source"
+        case verify = "verify"
+        case threeDS2Enabled = "enable_three_d_s_2"
     }
     
     public init(source: String) {
--- ProcessOut/Classes/TokenRequest.swift
+++ ProcessOut/Classes/TokenRequest.swift
@@ -9,11 +9,12 @@ import Foundation
 
 class TokenRequest: Encodable {
     var source: String = ""
-    var verify: Bool = true
-    
+    var verify = true
+    var enable3DS2 = true
     enum CodingKeys: String, CodingKey {
         case source = "source"
         case verify = "verify"
+        case enable3DS2 = "enable_three_d_s_2"
     }
     
     init(source: String) {
