9339182b56704c11333905c6d9d7db51425fd240
simeon-kostadinov-cko <simeon.kostadinov@checkout.com>
2022-02-07 11:13:13 +0000

    Bump SDK version to v2.13.0

--- ProcessOut/Classes/ProcessOut.swift
+++ ProcessOut/Classes/ProcessOut.swift
@@ -76,7 +76,7 @@ public class ProcessOut {
         }
     }
 
-    static let ApiVersion: String = "v2.12.1"
+    static let ApiVersion: String = "v2.13.0"
     private static let ApiUrl: String = "https://api.processout.com"
     internal static let CheckoutUrl: String = "https://checkout.processout.com"
     internal static var ProjectId: String?
