0a7efb79ba5f65b8d8a7abf9fee48a7e5c575292
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2022-12-22 15:36:52 +0100

    Bump versions

--- ProcessOut/Classes/ProcessOut.swift
+++ ProcessOut/Classes/ProcessOut.swift
@@ -76,7 +76,7 @@ public class ProcessOut {
         }
     }
 
-    static let ApiVersion: String = "v2.15.4"
+    static let ApiVersion: String = "v2.16.4"
     private static let ApiUrl: String = "https://api.processout.com"
     internal static let CheckoutUrl: String = "https://checkout.processout.com"
     internal static var ProjectId: String?
