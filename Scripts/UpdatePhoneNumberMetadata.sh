f9defbfe8091e65ce2c37ba13c93209f286b042d
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-03-23 22:00:03 +0100

    Add update phone number metadata script

---
+++ Scripts/UpdatePhoneNumberMetadata.sh
@@ -0,0 +1,28 @@
+
+#!/bin/sh
+
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+OUTPUT_DIR="$SCRIPT_DIR/../Sources/ProcessOut/Resources/PhoneNumberMetadata"
+
+# Validates arguments acount
+test $# -eq 1
+
+# Go to temporary directory
+cd $(mktemp -d)
+
+# Clone Google's libphonenumber
+git clone --depth 1 --branch $1 https://github.com/google/libphonenumber libphonenumber
+
+mkdir -p $OUTPUT_DIR
+
+# Convert XML metadata to JSON
+"$SCRIPT_DIR/ConvertPhoneNumberMetadata.swift" "libphonenumber/resources/PhoneNumberMetadata.xml" "$OUTPUT_DIR/PhoneNumberMetadata.json"
+
+# Write metadata
+echo $1 >> "$OUTPUT_DIR/PhoneNumberMetadata.version"
+
+function cleanup {
+  rm -rf $WORK_DIR
+}
+
+trap cleanup EXIT
