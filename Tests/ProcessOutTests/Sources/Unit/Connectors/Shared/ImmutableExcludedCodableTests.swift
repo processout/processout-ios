3258dbe251275e4402dea397d02ab71eb827659c
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-03-29 20:19:49 +0200

    Add more tests

---
+++ Tests/ProcessOutTests/Sources/Unit/Connectors/Shared/ImmutableExcludedCodableTests.swift
@@ -0,0 +1,34 @@
+//
+//  ImmutableExcludedCodableTests.swift
+//  ProcessOutTests
+//
+//  Created by Andrii Vysotskyi on 29.03.2023.
+//
+
+import XCTest
+@testable import ProcessOut
+
+final class ImmutableExcludedCodableTests: XCTestCase {
+
+    override func setUp() {
+        super.setUp()
+    }
+
+    func test_excludedCodable_whenWrappedInContainer_isNotEncoded() throws {
+        // Given
+        let encoder = JSONEncoder()
+        let value = Container(value: POImmutableExcludedCodable(value: 1))
+
+        // When
+        let encodeData = try encoder.encode(value)
+
+        // Then
+        XCTAssertEqual(Data("{}".utf8), encodeData)
+    }
+}
+
+private struct Container: Encodable {
+
+    @POImmutableExcludedCodable
+    var value: Int
+}
