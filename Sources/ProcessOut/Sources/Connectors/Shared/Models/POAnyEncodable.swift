4c8fb8d107f6db6887e488a83797bd299a58710b
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-03-29 20:18:30 +0200

    Document any encodable

--- Sources/ProcessOut/Sources/Connectors/Shared/Models/POAnyEncodable.swift
+++ Sources/ProcessOut/Sources/Connectors/Shared/Models/POAnyEncodable.swift
@@ -7,6 +7,18 @@
 
 import Foundation
 
+/// A type-erased `Encodable` value.
+///
+/// The `POAnyEncodable` type forwards encode operation to an underlying encodable value, hiding the type of the
+/// wrapped value.
+///
+/// You can store mixed-type values in dictionaries and other collections that require Encodable conformance by
+/// wrapping mixed-type values in AnyEncodable instances:
+///
+/// ```swift
+/// let array = [AnyEncodable(123), AnyEncodable("456")]
+/// try JSONEncoder().encode(array)
+/// ```
 public struct POAnyEncodable: Encodable {
 
     private let encoding: (Encoder) throws -> Void
