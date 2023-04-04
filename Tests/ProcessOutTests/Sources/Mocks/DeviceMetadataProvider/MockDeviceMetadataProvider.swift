d361808ba02d3ecfc2e9f1c7617ccd407cbe97a8
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-04-04 18:37:58 +0200

    Add mock device metadata provider

---
+++ Tests/ProcessOutTests/Sources/Mocks/DeviceMetadataProvider/MockDeviceMetadataProvider.swift
@@ -0,0 +1,13 @@
+//
+//  MockDeviceMetadataProvider.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 04.04.2023.
+//
+
+@testable import ProcessOut
+
+struct MockDeviceMetadataProvider: DeviceMetadataProviderType {
+
+    let deviceMetadata: DeviceMetadata
+}
