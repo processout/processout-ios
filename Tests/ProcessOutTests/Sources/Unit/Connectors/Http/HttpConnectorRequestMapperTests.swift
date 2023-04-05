80d15c11e36d16a91f6638f226e0787b9ed9f2ae
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-04-05 13:03:24 +0200

    Finalize mapper tests

--- Tests/ProcessOutTests/Sources/Unit/Connectors/Http/HttpConnectorRequestMapperTests.swift
+++ Tests/ProcessOutTests/Sources/Unit/Connectors/Http/HttpConnectorRequestMapperTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 final class HttpConnectorRequestMapperTests: XCTestCase {
 
+    // MARK: - URL
+
     func test_urlRequest_whenBaseUrlIsMalformed_fails() throws {
         // Given
         let configuration = HttpConnectorRequestMapperConfiguration(
@@ -24,24 +26,20 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertThrowsError(try sut.urlRequest(from: request))
     }
 
-    func test_urlRequest_whenRequestPathIsInvalid_fails() throws {
+    // MARK: - Request Path
+
+    func test_urlRequest_whenPathIsInvalid_fails() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "|")
 
         // Then
         XCTAssertThrowsError(try sut.urlRequest(from: request))
     }
 
-    func test_urlRequest_whenRequestPathIsValid_returnsRequestWithSamePath() throws {
+    func test_urlRequest_whenPathIsValid_succeeds() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "/test/path")
 
         // When
@@ -51,16 +49,12 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertEqual(urlRequest.url?.path(), request.path)
     }
 
-    // MARK: - Body
+    // MARK: - Request Body
 
     func test_urlRequest_whenBodyIsSet_encodesBody() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
-        let body = "body"
-        let request = HttpConnectorRequest<VoidCodable>.post(path: "", body: body)
+        let sut = createMapper(configuration: defaultConfiguration)
+        let request = HttpConnectorRequest<VoidCodable>.post(path: "", body: "body")
 
         // When
         let urlRequest = try sut.urlRequest(from: request)
@@ -69,12 +63,9 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertEqual(urlRequest.httpBody, Data(#""body""#.utf8))
     }
 
-    func test_urlRequest_whenRequestIncludesDeviceMetadataButBodyIsNotSet_encodesBody() throws {
+    func test_urlRequest_whenIncludesDeviceMetadataButBodyIsNotSet_encodesBody() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.post(path: "", includesDeviceMetadata: true)
 
         // When
@@ -82,34 +73,29 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
         // Then
         // swiftlint:disable:next line_length
-        let expectedBody = #"{"device":{"appLanguage":"language","appScreenHeight":2,"appScreenWidth":1,"appTimeZoneOffset":3,"channel":"test"}}"#
+        let expectedBody = #"{"device":{"appLanguage":"en","appScreenHeight":2,"appScreenWidth":1,"appTimeZoneOffset":3,"channel":"test"}}"#
         XCTAssertEqual(urlRequest.httpBody, Data(expectedBody.utf8))
     }
 
-    func test_urlRequest_whenBodyIsSetAndRequestIncludesDeviceMetadata_encodesBody() throws {
+    func test_urlRequest_whenIncludesDeviceMetadataAndBodyIsSet_encodesBody() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
+        let sut = createMapper(configuration: defaultConfiguration)
+        let request = HttpConnectorRequest<VoidCodable>.post(
+            path: "", body: ["key": "value"], includesDeviceMetadata: true
         )
-        let sut = createMapper(configuration: configuration)
-        let body = ["key": "value"]
-        let request = HttpConnectorRequest<VoidCodable>.post(path: "", body: body, includesDeviceMetadata: true)
 
         // When
         let urlRequest = try sut.urlRequest(from: request)
 
         // Then
         // swiftlint:disable:next line_length
-        let expectedBody = #"{"device":{"appLanguage":"language","appScreenHeight":2,"appScreenWidth":1,"appTimeZoneOffset":3,"channel":"test"},"key":"value"}"#
+        let expectedBody = #"{"device":{"appLanguage":"en","appScreenHeight":2,"appScreenWidth":1,"appTimeZoneOffset":3,"channel":"test"},"key":"value"}"#
         XCTAssertEqual(urlRequest.httpBody, Data(expectedBody.utf8))
     }
 
     func test_urlRequest_whenBodyIsNotSet_returnsRequestWithoutBody() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -121,31 +107,18 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
     func test_urlRequest_whenBodyIsInvalid_fails() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.post(path: "", body: Float.infinity)
 
         // Then
-        XCTAssertThrowsError(try sut.urlRequest(from: request)) { error in
-            switch error {
-            case HttpConnectorFailure.coding:
-                break
-            default:
-                XCTFail("Unexpected failure")
-            }
-        }
+        XCTAssertThrowsError(try sut.urlRequest(from: request))
     }
 
-    // MARK: - Query
+    // MARK: - Request Query
 
-    func test_urlRequest_whenQueryIsSet_returnsRequestWithQuery() throws {
+    func test_urlRequest_whenQueryIsSet_succeeds() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "", query: ["key": "value"])
 
         // When
@@ -155,14 +128,11 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertEqual(urlRequest.url?.query(), "key=value")
     }
 
-    // MARK: - Headers
+    // MARK: - Request Headers
 
-    func test_urlRequest_returnsRequestWithValidUserAgent() throws {
+    func test_urlRequest_addsUserAgent() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: "X-Y-Z"
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -170,16 +140,13 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
         // Then
         let userAgent = urlRequest.value(forHTTPHeaderField: "user-agent")
-        let userAgentRegex = /^iOS\/Version\/.*\/ProcessOut iOS-Bindings\/X-Y-Z$/
+        let userAgentRegex = /^iOS\/Version\/.*\/ProcessOut iOS-Bindings\/1\.2\.3$/
         XCTAssertNotNil(userAgent?.firstMatch(of: userAgentRegex))
     }
 
     func test_urlRequest_whenPrivateKeyIsNotRequired_addsAuthorization() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "PROJECT_ID", privateKey: "PRIVATE_KEY", version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -187,15 +154,12 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
         // Then
         let authorization = urlRequest.value(forHTTPHeaderField: "Authorization")
-        XCTAssertEqual(authorization, "Basic UFJPSkVDVF9JRDo=")
+        XCTAssertEqual(authorization, "Basic PElEPjo=")
     }
 
     func test_urlRequest_whenPrivateKeyIsRequired_addsAuthorization() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "PROJECT_ID", privateKey: "PRIVATE_KEY", version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "", requiresPrivateKey: true)
 
         // When
@@ -203,13 +167,13 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
         // Then
         let authorization = urlRequest.value(forHTTPHeaderField: "Authorization")
-        XCTAssertEqual(authorization, "Basic UFJPSkVDVF9JRDpQUklWQVRFX0tFWQ==")
+        XCTAssertEqual(authorization, "Basic PElEPjo8S0VZPg==")
     }
 
     func test_urlRequest_whenPrivateKeyIsRequiredButNotSet_fails() throws {
         // Given
         let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "PROJECT_ID", privateKey: nil, version: ""
+            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
         )
         let sut = createMapper(configuration: configuration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "", requiresPrivateKey: true)
@@ -220,10 +184,7 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
 
     func test_urlRequest_addsDefaultHeaders() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -236,12 +197,9 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         }
     }
 
-    func test_urlRequest_addsValidIdempotencyKey() throws {
+    func test_urlRequest_addsValidIdempotencyKeyHeader() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -252,12 +210,9 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertEqual(idempotencyKey, request.id)
     }
 
-    func test_urlRequest_addsValidJsonContentTypeKey() throws {
+    func test_urlRequest_addsJsonContentTypeHeader() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
+        let sut = createMapper(configuration: defaultConfiguration)
         let request = HttpConnectorRequest<VoidCodable>.get(path: "")
 
         // When
@@ -268,19 +223,16 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
         XCTAssertEqual(idempotencyKey, "application/json")
     }
 
-    func test_urlRequest_whenRequestHeaderIsSet_addsIt() throws {
+    func test_urlRequest_whenRequestHeaderIsSet_overridesDefaultHeader() throws {
         // Given
-        let configuration = HttpConnectorRequestMapperConfiguration(
-            baseUrl: Constants.baseUrl, projectId: "", privateKey: nil, version: ""
-        )
-        let sut = createMapper(configuration: configuration)
-        let request = HttpConnectorRequest<VoidCodable>.get(path: "", headers: ["key": "value"])
+        let sut = createMapper(configuration: defaultConfiguration)
+        let request = HttpConnectorRequest<VoidCodable>.get(path: "", headers: ["Content-Type": "test"])
 
         // When
         let urlRequest = try sut.urlRequest(from: request)
 
         // Then
-        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "key"), "value")
+        XCTAssertEqual(urlRequest.value(forHTTPHeaderField: "Content-Type"), "test")
     }
 
     // MARK: - Private Nested Types
@@ -294,19 +246,16 @@ final class HttpConnectorRequestMapperTests: XCTestCase {
     private func createMapper(configuration: HttpConnectorRequestMapperConfiguration) -> HttpConnectorRequestMapper {
         let encoder = JSONEncoder()
         encoder.outputFormatting = .sortedKeys
-        let mockDeviceMetadata = DeviceMetadata(
-            appLanguage: "language",
-            appScreenWidth: 1,
-            appScreenHeight: 2,
-            appTimeZoneOffset: 3,
-            channel: "test"
-        )
         let mapper = HttpConnectorRequestMapper(
             configuration: configuration,
             encoder: encoder,
-            deviceMetadataProvider: MockDeviceMetadataProvider(deviceMetadata: mockDeviceMetadata),
+            deviceMetadataProvider: StubDeviceMetadataProvider(),
             logger: POLogger()
         )
         return mapper
+    }
+
+    private var defaultConfiguration: HttpConnectorRequestMapperConfiguration {
+        .init(baseUrl: Constants.baseUrl, projectId: "<ID>", privateKey: "<KEY>", version: "1.2.3")
     }
 }
