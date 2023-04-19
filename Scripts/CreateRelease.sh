25a2b7ea7ad4c097a332aa839a0929dc509d8a58
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-04-19 17:19:41 +0200

    feat(ad-hoc): improve release script (#82)
    * Fix version in ProcessOutCheckout3DS.podspec
    * Extract release creation logic into separate script
    * Bonus: remove dependency to softprops/action-gh-release

---
+++ Scripts/CreateRelease.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+# Creates XCFramework
+source Scripts/CreateXcframework.sh
+
+# Builds documentation
+source Scripts/CreateDocumentation.sh
+
+# Reads current version into variable
+RELEASE_VERSION=$(cat Version.resolved)
+
+# Creates release
+gh release create $RELEASE_VERSION \
+  --generate-notes \
+  --verify-tag \
+  .build/framework/*.xcframework.zip \
+  .build/documentation/*.doccarchive.zip
+
+# Push Podspecs
+for PRODUCT in "ProcessOut" "ProcessOutCheckout3DS"; do
+    pod trunk push $PRODUCT.podspec --allow-warnings
+done
