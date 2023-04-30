4258d8a55985743e4aa0ce693a99355e6b99fb56
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-04-30 10:21:27 +0200

    Add diffable data source backport

---
+++ Sources/ProcessOut/Sources/UI/Shared/Utils/CollectionDiffableDataSource/DiffableDataSourceSnapshot.swift
@@ -0,0 +1,46 @@
+//
+//  DiffableDataSourceSnapshot.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 30.04.2023.
+//
+
+struct DiffableDataSourceSnapshot<SectionIdentifier: Hashable, ItemIdentifier: Hashable> {
+
+    private(set) var sectionIdentifiers: [SectionIdentifier] = []
+
+    mutating func appendSections(_ identifiers: [SectionIdentifier]) {
+        sectionIdentifiers += identifiers
+    }
+
+    mutating func appendItems(_ identifiers: [ItemIdentifier], toSection sectionIdentifier: SectionIdentifier) {
+        if let itemIdentifiers = self._itemIdentifiers[sectionIdentifier] {
+            self._itemIdentifiers[sectionIdentifier] = itemIdentifiers + identifiers
+        } else {
+            self._itemIdentifiers[sectionIdentifier] = identifiers
+        }
+    }
+
+    var numberOfSections: Int {
+        sectionIdentifiers.count
+    }
+
+    func numberOfItems(inSection identifier: SectionIdentifier) -> Int {
+        _itemIdentifiers[identifier]?.count ?? 0
+    }
+
+    func sectionIdentifier(for index: Int) -> SectionIdentifier? {
+        if sectionIdentifiers.indices.contains(index) {
+            return sectionIdentifiers[index]
+        }
+        return nil
+    }
+
+    func itemIdentifiers(inSection identifier: SectionIdentifier) -> [ItemIdentifier] {
+        _itemIdentifiers[identifier] ?? []
+    }
+
+    // MARK: -
+
+    private var _itemIdentifiers: [SectionIdentifier: [ItemIdentifier]] = [:]
+}
