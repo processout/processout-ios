ac448c4b7b1f13e89d5abe59baf52885255b2fd5
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-05-19 16:39:48 +0200

    Update input form style

--- Sources/ProcessOut/Sources/UI/Shared/DesignSystem/Views/InputForm/POInputFormStyle.swift
+++ Sources/ProcessOut/Sources/UI/Shared/DesignSystem/Views/InputForm/POInputFormStyle.swift
@@ -25,43 +25,43 @@ extension POInputFormStyle {
 
     /// Default input form style.
     public static let `default` = Self(
-        normal: normal(fieldTypography: .bodyDefault1), error: error(fieldTypography: .bodyDefault1)
+        normal: normal(fieldTypography: .Fixed.label), error: error(fieldTypography: .Fixed.label)
     )
 
     /// Default code input form style.
     public static let code = Self(
-        normal: normal(fieldTypography: .title), error: error(fieldTypography: .title)
+        normal: normal(fieldTypography: .Medium.title), error: error(fieldTypography: .Medium.title)
     )
 
     // MARK: - Private Methods
 
     private static func normal(fieldTypography: POTypography) -> POInputFormStateStyle {
         POInputFormStateStyle(
-            title: .init(color: Asset.Colors.Text.primary.color, typography: .bodyLarge),
+            title: .init(color: Asset.Colors.New.Text.secondary.color, typography: .Fixed.labelHeading),
             field: .init(
-                text: .init(color: Asset.Colors.Text.primary.color, typography: fieldTypography),
-                placeholder: .init(color: Asset.Colors.Text.secondary.color, typography: fieldTypography),
-                backgroundColor: Asset.Colors.Background.input.color,
-                border: .init(radius: 8, width: 1, color: Asset.Colors.Border.primary.color),
+                text: .init(color: Asset.Colors.New.Text.primary.color, typography: fieldTypography),
+                placeholder: .init(color: Asset.Colors.New.Text.muted.color, typography: fieldTypography),
+                backgroundColor: Asset.Colors.New.Surface.background.color,
+                border: .regular(radius: 8, color: Asset.Colors.New.Border.default.color),
                 shadow: .clear,
-                tintColor: Asset.Colors.Text.primary.color
+                tintColor: Asset.Colors.New.Text.primary.color
             ),
-            description: .init(color: Asset.Colors.Text.secondary.color, typography: .bodySmall2)
+            description: .init(color: Asset.Colors.New.Text.muted.color, typography: .Fixed.label)
         )
     }
 
     private static func error(fieldTypography: POTypography) -> POInputFormStateStyle {
         POInputFormStateStyle(
-            title: .init(color: Asset.Colors.Text.primary.color, typography: .bodyLarge),
+            title: .init(color: Asset.Colors.New.Text.secondary.color, typography: .Fixed.labelHeading),
             field: .init(
-                text: .init(color: Asset.Colors.Text.primary.color, typography: fieldTypography),
-                placeholder: .init(color: Asset.Colors.Text.secondary.color, typography: fieldTypography),
-                backgroundColor: Asset.Colors.Background.input.color,
-                border: .init(radius: 8, width: 1, color: Asset.Colors.Border.error.color),
+                text: .init(color: Asset.Colors.New.Text.primary.color, typography: fieldTypography),
+                placeholder: .init(color: Asset.Colors.New.Text.muted.color, typography: fieldTypography),
+                backgroundColor: Asset.Colors.New.Surface.background.color,
+                border: .init(radius: 8, width: 1, color: Asset.Colors.New.Text.error.color),
                 shadow: .clear,
-                tintColor: Asset.Colors.Text.error.color
+                tintColor: Asset.Colors.New.Text.error.color
             ),
-            description: .init(color: Asset.Colors.Text.error.color, typography: .bodySmall2)
+            description: .init(color: Asset.Colors.New.Text.error.color, typography: .Fixed.label)
         )
     }
 }
