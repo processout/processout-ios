09a5f75baecdef6be28660a3dd5f8d4cb50088ec
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-05-24 16:10:57 +0200

    Remove unwanted default implementation

--- Sources/ProcessOut/Sources/UI/Modules/NativeAlternativePaymentMethod/View/Layout/NativeAlternativePaymentMethodCollectionLayoutDelegate.swift
+++ Sources/ProcessOut/Sources/UI/Modules/NativeAlternativePaymentMethod/View/Layout/NativeAlternativePaymentMethodCollectionLayoutDelegate.swift
@@ -16,10 +16,3 @@ protocol NativeAlternativePaymentMethodCollectionLayoutDelegate: AnyObject, UICo
     /// Asks delegate whether cell at given index should be decorated with separator.
     func collectionViewLayout(_ layout: UICollectionViewLayout, shouldSeparateCellAt indexPath: IndexPath) -> Bool
 }
-
-extension NativeAlternativePaymentMethodCollectionLayoutDelegate {
-
-    func collectionViewLayout(_ layout: UICollectionViewLayout, shouldSeparateCellAt indexPath: IndexPath) -> Bool {
-        false
-    }
-}
