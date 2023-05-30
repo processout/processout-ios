a9b21802cd2cb3ffbeb38f280d9e211bd5802fe7
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-05-30 16:47:26 +0200

    Use checkout 3ds service

--- Example/Example/Sources/UI/Modules/CardPayment/CardPaymentBuilder.swift
+++ Example/Example/Sources/UI/Modules/CardPayment/CardPaymentBuilder.swift
@@ -7,21 +7,41 @@
 
 import UIKit
 import ProcessOut
+import ProcessOutCheckout3DS
 
 final class CardPaymentBuilder {
 
     func build() -> UIViewController {
-        let threeDSService = POTest3DSService()
+        let checkoutDelegate = Checkout3DSServiceDelegate()
         let router = CardPaymentRouter()
         let viewModel = CardPaymentViewModel(
             router: router,
             invoicesService: ProcessOut.shared.invoices,
             cardsService: ProcessOut.shared.cards,
-            threeDSService: threeDSService
+            threeDSService: POCheckout3DSServiceBuilder.with(delegate: checkoutDelegate).build()
         )
         let viewController = CardPaymentViewController(viewModel: viewModel)
-        threeDSService.viewController = viewController
+        checkoutDelegate.viewController = viewController
         router.viewController = viewController
         return viewController
+    }
+}
+
+final class Checkout3DSServiceDelegate: POCheckout3DSServiceDelegate {
+
+    unowned var viewController: UIViewController! // swiftlint:disable:this implicitly_unwrapped_optional
+
+    // MARK: - POCheckout3DSServiceDelegate
+
+    func handle(redirect: PO3DSRedirect, completion: @escaping (Result<String, POFailure>) -> Void) {
+        let viewController = PO3DSRedirectViewControllerBuilder
+            .with(redirect: redirect)
+            .with { [weak self] result in
+                self?.viewController.dismiss(animated: true) {
+                    completion(result)
+                }
+            }
+            .build()
+        self.viewController.present(viewController, animated: true)
     }
 }
