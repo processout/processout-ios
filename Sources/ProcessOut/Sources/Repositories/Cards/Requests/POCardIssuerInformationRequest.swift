fba58f908a8376e9d68d092af48f1d035ac9f185
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-06-20 13:26:48 +0200

    Expose request init

--- Sources/ProcessOut/Sources/Repositories/Cards/Requests/POCardIssuerInformationRequest.swift
+++ Sources/ProcessOut/Sources/Repositories/Cards/Requests/POCardIssuerInformationRequest.swift
@@ -10,4 +10,9 @@ public struct POCardIssuerInformationRequest {
 
     /// Card identification number. Corresponds to the first 6 or 8 digits of the main card number.
     public let iin: String
+
+    /// Creates request instance.
+    public init(iin: String) {
+        self.iin = iin
+    }
 }
