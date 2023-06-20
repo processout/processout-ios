884173ce0d49425ac5a8af052e2866102352bcae
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-06-20 16:31:18 +0200

    Support escaping plain text

--- Sources/ProcessOut/Sources/Core/Markdown/MarkdownParser.swift
+++ Sources/ProcessOut/Sources/Core/Markdown/MarkdownParser.swift
@@ -5,6 +5,7 @@
 //  Created by Andrii Vysotskyi on 13.06.2023.
 //
 
+import Foundation
 @_implementationOnly import cmark
 
 final class MarkdownParser {
@@ -17,5 +18,25 @@ final class MarkdownParser {
             preconditionFailure("Failed to parse markdown document")
         }
         return MarkdownDocument(cmarkNode: document)
+    }
+
+    /// Escapes given plain text so it can be represented as is in markdown.
+    static func escaped(plainText: String) -> String {
+        var markdown = String()
+        markdown.reserveCapacity(plainText.count)
+        for character in plainText {
+            if character.unicodeScalars.allSatisfy(Constants.specialCharacters.contains) {
+                markdown += Constants.escapeCharacter
+            }
+            markdown += String(character)
+        }
+        return markdown
+    }
+
+    // MARK: - Private Nested Types
+
+    private enum Constants {
+        static let specialCharacters = CharacterSet(charactersIn: "\\`*_{}[]()#+-.!")
+        static let escapeCharacter = "\\"
     }
 }
