888f83a4da4df68f5143e663fb605878d7c036fb
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-06-22 14:18:27 +0200

    Revert unwanted change

--- Example/Example/Sources/UI/Modules/AlternativePayment/Methods/Interactor/AlternativePaymentMethodsInteractor.swift
+++ Example/Example/Sources/UI/Modules/AlternativePayment/Methods/Interactor/AlternativePaymentMethodsInteractor.swift
@@ -99,8 +99,7 @@ final class AlternativePaymentMethodsInteractor:
             name: UUID().uuidString,
             amount: amount.description,
             currency: currencyCode,
-            returnUrl: Example.Constants.returnUrl,
-            customerId: "cust_jEVryalQVtdvJ2zc66uMI0D5CfvEayQO"
+            returnUrl: Example.Constants.returnUrl
         )
         invoicesService.createInvoice(request: request) { [weak self] result in
             self?.state = .started(startedState)
