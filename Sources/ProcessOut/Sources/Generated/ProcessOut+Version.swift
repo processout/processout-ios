59e44da57151dd47eb5d9a632029cbe2f8a26600
andrii-vysotskyi-cko <andrii-vysotskyi-cko@users.noreply.github.com>
2023-06-23 07:41:33 +0000

    Update version

--- Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
+++ Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
@@ -7,8 +7,8 @@
 
 extension ProcessOut {
 
-    /// The current version of this library, value is "4.0.1".
+    /// The current version of this library, value is "4.1.0".
     public static var version: String {
-        "4.0.1"
+        "4.1.0"
     }
 }
