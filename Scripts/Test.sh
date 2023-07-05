d620b4b26cb562b6a1d2495851428463e7a2e842
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-07-05 16:13:39 +0200

    WIP

--- Scripts/Test.sh
+++ Scripts/Test.sh
@@ -5,16 +5,20 @@ set -euo pipefail
 SDK='iphonesimulator'
 DESTINATION='platform=iOS Simulator,name=iPhone 14 Pro,OS=latest'
 
-xcodebuild clean test \
-    -project ProcessOut.xcodeproj \
-    -scheme ProcessOut \
-    -sdk "$SDK" \
-    -destination "$DESTINATION" |
-    bundle exec xcpretty
+export TARGET_ROOT="$(pwd)/Tests/ProcessOutTests/"
 
-# Checkout3DS dependency is only available via cocoapods now, so target is tested
-# using `pod lib lint`.
-bundle exec pod repo update
-bundle exec pod lib lint ProcessOutCheckout3DS.podspec --allow-warnings
+source Scripts/SwiftGen.sh
+
+# xcodebuild clean test \
+#     -project ProcessOut.xcodeproj \
+#     -scheme ProcessOut \
+#     -sdk "$SDK" \
+#     -destination "$DESTINATION" |
+#     bundle exec xcpretty
+
+# # Checkout3DS dependency is only available via cocoapods now, so target is tested
+# # using `pod lib lint`.
+# bundle exec pod repo update
+# bundle exec pod lib lint ProcessOutCheckout3DS.podspec --allow-warnings
 
 # todo(andrii-vysotskyi): run example target tests when POM-144 is resolved
