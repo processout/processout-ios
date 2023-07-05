adf3791d1a7052f033d96d74c44cc7a1e3d6b860
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-07-05 12:07:17 +0200

    Remove comment

--- Package.swift
+++ Package.swift
@@ -17,8 +17,6 @@ let package = Package(
             dependencies: [
                 .target(name: "cmark")
             ],
-            // Having DocC in sources may cause build failures so excluded until issue is resolved by Apple. See
-            // https://forums.swift.org/t/xcode-and-swift-package-manager/44704 for details.
             exclude: ["ProcessOut.docc", "swiftgen.yml"],
             resources: [
                 .process("Resources")
