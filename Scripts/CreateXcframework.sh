f2d5af0d7aab26ec41fb89fd5b1b14d49a7c6c43
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-07-06 12:59:28 +0200

    Improve frameworks generation

--- Scripts/CreateXcframework.sh
+++ Scripts/CreateXcframework.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-set -euo pipefail
-
 OUTPUT_DIR=".build/framework"
 
 function build_framework {(
-    set -e
+    set -euo pipefail
 
     xcodebuild archive \
         -scheme $1 \
@@ -29,7 +27,7 @@ function build_framework {(
     zip $1.xcframework.zip -r $1.xcframework -x '.*' -x '__MACOSX'
 )}
 
-set -e
+set -euo pipefail
 
 rm -rf $OUTPUT_DIR
 mkdir -p $OUTPUT_DIR
