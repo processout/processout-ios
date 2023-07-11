f3d9e66dce569517ed1e5a805c8409cb42bf11e2
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-07-11 16:28:15 +0200

    Add annotation to marker protocol

--- Sources/ProcessOut/Sources/Core/Utils/POAutoAsync.swift
+++ Sources/ProcessOut/Sources/Core/Utils/POAutoAsync.swift
@@ -9,4 +9,4 @@
 /// automatically generate `async` methods for their callback-based counterparts. Only methods where last argument
 /// is a closure that matches `(Result<?, Error>) -> Void` type are picked, see `Templates/AutoAsync.stencil` in
 /// project's root directory for details.
-public protocol POAutoAsync {}
+@_marker public protocol POAutoAsync { }
