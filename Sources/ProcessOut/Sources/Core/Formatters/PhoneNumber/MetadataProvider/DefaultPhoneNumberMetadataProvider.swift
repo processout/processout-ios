7333d7a87ece35962b0e500bf2ad8b01d7e26b72
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-07-14 17:30:01 +0200

    Make phone number metadata provider prewarm async

--- Sources/ProcessOut/Sources/Core/Formatters/PhoneNumber/MetadataProvider/DefaultPhoneNumberMetadataProvider.swift
+++ Sources/ProcessOut/Sources/Core/Formatters/PhoneNumber/MetadataProvider/DefaultPhoneNumberMetadataProvider.swift
@@ -11,36 +11,55 @@ final class DefaultPhoneNumberMetadataProvider: PhoneNumberMetadataProvider {
 
     static let shared = DefaultPhoneNumberMetadataProvider()
 
+    /// - NOTE: Method is asynchronous.
+    func prewarm() {
+        prewarm(sync: false)
+    }
+
     // MARK: - PhoneNumberMetadataProvider
 
     func metadata(for countryCode: String) -> PhoneNumberMetadata? {
         let transformedCountryCode = countryCode.applyingTransform(.toLatin, reverse: false) ?? countryCode
-        return metadata[transformedCountryCode]
-    }
-
-    func prewarm() {
-        _ = metadata
-    }
-
-    // MARK: - Private Methods
-
-    private init() {
-        // NOP
+        if let metadata = metadata {
+            return metadata[transformedCountryCode]
+        }
+        prewarm(sync: true)
+        return metadata?[transformedCountryCode]
     }
 
     // MARK: - Private Properties
 
-    private lazy var metadata: [String: PhoneNumberMetadata] = {
-        let metadata: [PhoneNumberMetadata]
-        do {
-            let data = try Data(contentsOf: Files.phoneNumberMetadata.url)
-            metadata = try JSONDecoder().decode([PhoneNumberMetadata].self, from: data)
-        } catch {
-            return [:]
+    private let dispatchQueue: DispatchQueue
+
+    @UnfairlyLocked
+    private var metadata: [String: PhoneNumberMetadata]?
+
+    // MARK: - Private Methods
+
+    private init() {
+        dispatchQueue = DispatchQueue(label: "process-out.phone-number-metadata-provider", qos: .userInitiated)
+    }
+
+    private func prewarm(sync: Bool) {
+        let dispatchWorkItem = DispatchWorkItem { [weak self] in
+            guard let self, self.metadata == nil else {
+                return
+            }
+            let groupedMetadata: [String: PhoneNumberMetadata]
+            do {
+                let data = try Data(contentsOf: Files.phoneNumberMetadata.url)
+                let metadata = try JSONDecoder().decode([PhoneNumberMetadata].self, from: data)
+                groupedMetadata = Dictionary(grouping: metadata, by: \.countryCode).compactMapValues { values in
+                    let countryCode = values.first!.countryCode // swiftlint:disable:this force_unwrapping
+                    return PhoneNumberMetadata(countryCode: countryCode, formats: values.flatMap(\.formats))
+                }
+            } catch {
+                assertionFailure("Failed to load metadata: \(error.localizedDescription)")
+                groupedMetadata = [:]
+            }
+            self.$metadata.withLock { $0 = groupedMetadata }
         }
-        return Dictionary(grouping: metadata, by: \.countryCode).compactMapValues { values in
-            let countryCode = values.first!.countryCode // swiftlint:disable:this force_unwrapping
-            return PhoneNumberMetadata(countryCode: countryCode, formats: values.flatMap(\.formats))
-        }
-    }()
+        let executor = sync ? dispatchQueue.sync : dispatchQueue.async
+        executor(dispatchWorkItem)
+    }
 }
