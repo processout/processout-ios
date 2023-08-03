db8b87119a7a71cbff0379655564edd6d303ae93
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-08-03 15:43:27 +0200

    Remove mint dependency for example project

--- Example/Scripts/BootstrapProject.sh
+++ Example/Scripts/BootstrapProject.sh
@@ -5,13 +5,8 @@ set -euo pipefail
 # Installs brew dependencies
 brew bundle -q
 
-# Installs mint dependencies if needed
-if ! [[ "$@" =~ '--skip-mint-bootstrap' ]]; then
-    mint bootstrap
-fi
-
 # Generates project
-mint run xcodegen generate
+xcodegen generate
 
 # Creates hardlink to Package.resolved
 SWIFTPM_DIR='Example.xcodeproj/project.xcworkspace/xcshareddata/swiftpm'
