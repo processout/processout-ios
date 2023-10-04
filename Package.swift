c35797a1ef92c628335852e169e26e2abc8e199c
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-10-04 12:27:58 +0200

    Add Checkout3DS wrapper package

--- Package.swift
+++ Package.swift
@@ -10,6 +10,11 @@ let package = Package(
     ],
     products: [
         .library(name: "ProcessOut", targets: ["ProcessOut"]),
+        .library(name: "ProcessOutCheckout3DS", targets: ["ProcessOutCheckout3DS"])
+    ],
+    dependencies: [
+        // todo(andrii-vysotskyi): replace branch with version where feature is released.
+        .package(url: "https://github.com/checkout/checkout-3ds-sdk-ios.git", branch: "feature/PIMOB-2035_support_spm")
     ],
     targets: [
         .target(
@@ -21,6 +26,14 @@ let package = Package(
             resources: [
                 .process("Resources")
             ]
+        ),
+        .target(
+            name: "ProcessOutCheckout3DS",
+            dependencies: [
+                .target(name: "ProcessOut"),
+                .product(name: "Checkout3DSPackages", package: "checkout-3ds-sdk-ios")
+            ],
+            exclude: ["ProcessOutCheckout3DS.docc"]
         ),
         .binaryTarget(name: "cmark", path: "Vendor/cmark.xcframework")
     ]
