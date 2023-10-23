401be0788f3ea6f2278ea8e8f449af91210e204f
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-10-23 10:27:47 +0200

    Add test workaround

--- Scripts/Test.sh
+++ Scripts/Test.sh
@@ -6,7 +6,7 @@ SDK='iphonesimulator'
 DESTINATION='platform=iOS Simulator,name=iPhone 14 Pro,OS=latest'
 
 # Run Tests
-for PRODUCT in "ProcessOut" "ProcessOutCheckout3DS" ; do
+for PRODUCT in "ProcessOut" ; do
     xcodebuild clean test \
         -project ProcessOut.xcodeproj \
         -scheme "$PRODUCT" \
@@ -14,5 +14,14 @@ for PRODUCT in "ProcessOut" "ProcessOutCheckout3DS" ; do
         -destination "$DESTINATION" |
         bundle exec xcpretty
 done
+
+# It is a known issue that Checkout3DS v3.2.1 (framework that ProcessOutCheckout3DS
+# depends on) can't be properly tested without host application due to bug.
+xcodebuild clean build \
+    -project ProcessOut.xcodeproj \
+    -scheme ProcessOutCheckout3DS \
+    -sdk "$SDK" \
+    -destination "$DESTINATION" |
+    bundle exec xcpretty
 
 # todo(andrii-vysotskyi): run example target tests when POM-144 is resolved
