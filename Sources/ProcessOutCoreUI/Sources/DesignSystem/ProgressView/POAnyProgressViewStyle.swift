208ea3c0ba374fd5b22f4353ba315ca2ff1b2e5a
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-07 14:11:37 +0100

    Add any progress view style

---
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/ProgressView/POAnyProgressViewStyle.swift
@@ -0,0 +1,25 @@
+//
+//  POAnyProgressViewStyle.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 07.11.2023.
+//
+
+import Foundation
+
+/// The `POAnyProgressViewStyle` type forwards body creation to an underlying style value,
+/// hiding the type of the wrapped value.
+@_spi(PO) public struct POAnyProgressViewStyle: ProgressViewStyle {
+
+    public init(erasing style: any ProgressViewStyle) {
+        self.style = style
+    }
+
+    public func makeBody(configuration: Configuration) -> AnyView {
+        AnyView(style.makeBody(configuration: configuration))
+    }
+
+    // MARK: - Private Properties
+
+    private let style: any ProgressViewStyle
+}
