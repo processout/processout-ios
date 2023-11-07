45400e1286a670e8870e5fc80ac8a60a4db8b85a
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-07 14:15:25 +0100

    Fix build issue

--- Sources/ProcessOutCoreUI/Sources/DesignSystem/ProgressView/POAnyProgressViewStyle.swift
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/ProgressView/POAnyProgressViewStyle.swift
@@ -5,10 +5,11 @@
 //  Created by Andrii Vysotskyi on 07.11.2023.
 //
 
-import Foundation
+import SwiftUI
 
 /// The `POAnyProgressViewStyle` type forwards body creation to an underlying style value,
 /// hiding the type of the wrapped value.
+@available(iOS 14, *)
 @_spi(PO) public struct POAnyProgressViewStyle: ProgressViewStyle {
 
     public init(erasing style: any ProgressViewStyle) {
