7b5d70f5e1ab0420629bb9a44fcb2d7f11b2801d
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-08 14:48:38 +0100

    Remove progress view type eraser

---
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/ProgressView/View+ProgressViewStyle.swift
@@ -0,0 +1,19 @@
+//
+//  View+ProgressViewStyle.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 07.11.2023.
+//
+
+import SwiftUI
+
+extension View {
+
+    /// Sets the style for progress views in this view. This method should be used when
+    /// specific style type is unknown and there is no possibility to generic.
+    @_spi(PO)
+    @available(iOS 14, *)
+    public func poProgressViewStyle(_ style: any ProgressViewStyle) -> some View {
+        AnyView(self.progressViewStyle(style))
+    }
+}
