79cd2eba9d400a102042a881e04437b8526970b6
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-17 15:12:38 +0100

    Migrate APM module

---
+++ Sources/ProcessOutUI/Sources/Modules/AlternativePaymentMethod/AlternativePaymentMethodSafariViewModelDelegate.swift
@@ -0,0 +1,35 @@
+//
+//  AlternativePaymentMethodSafariViewModelDelegate.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 07.11.2022.
+//
+
+import Foundation
+import ProcessOut
+
+final class AlternativePaymentMethodSafariViewModelDelegate: DefaultSafariViewModelDelegate {
+
+    typealias Completion = (Result<POAlternativePaymentMethodResponse, POFailure>) -> Void
+
+    init(alternativePaymentMethodsService: POAlternativePaymentMethodsService, completion: @escaping Completion) {
+        self.alternativePaymentMethodsService = alternativePaymentMethodsService
+        self.completion = completion
+    }
+
+    // MARK: - DefaultSafariViewModelDelegate
+
+    func complete(with url: URL) throws {
+        let response = try alternativePaymentMethodsService.alternativePaymentMethodResponse(url: url)
+        completion(.success(response))
+    }
+
+    func complete(with failure: POFailure) {
+        completion(.failure(failure))
+    }
+
+    // MARK: - Private Properties
+
+    private let alternativePaymentMethodsService: POAlternativePaymentMethodsService
+    private let completion: Completion
+}
