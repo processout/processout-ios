d290e7a95ef8ee8c71534eb062c3f59db004117e
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-17 15:12:04 +0100

    Migrate safari module

---
+++ Sources/ProcessOutUI/Sources/Modules/Safari/DefaultSafariViewModelConfiguration.swift
@@ -0,0 +1,17 @@
+//
+//  DefaultSafariViewModelConfiguration.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 10.05.2023.
+//
+
+import Foundation
+
+struct DefaultSafariViewModelConfiguration {
+
+    /// Return url specified when creating invoice.
+    let returnUrl: URL
+
+    /// Optional timeout.
+    let timeout: TimeInterval?
+}
---
+++ Sources/ProcessOutUI/Sources/Modules/Safari/DefaultSafariViewModelDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  DefaultSafariViewModelDelegate.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 11.11.2022.
+//
+
+import Foundation
+import ProcessOut
+
+protocol DefaultSafariViewModelDelegate: AnyObject {
+
+    /// Asks delegate to complete with given url.
+    /// - Throws: error if transform is not possible for some reason.
+    func complete(with url: URL) throws
+
+    /// Completes with failure.
+    func complete(with failure: POFailure)
+}
