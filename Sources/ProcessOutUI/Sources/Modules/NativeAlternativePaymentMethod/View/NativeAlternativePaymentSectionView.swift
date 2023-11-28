4c363a8e3608fae4e90bc0e8a2a4781a0761a498
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-11-28 11:30:03 +0100

    Add section view

---
+++ Sources/ProcessOutUI/Sources/Modules/NativeAlternativePaymentMethod/View/NativeAlternativePaymentSectionView.swift
@@ -0,0 +1,38 @@
+//
+//  NativeAlternativePaymentSectionView.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 27.11.2023.
+//
+
+import SwiftUI
+@_spi(PO) import ProcessOutCoreUI
+
+@available(iOS 14, *)
+struct NativeAlternativePaymentSectionView: View {
+
+    let section: NativeAlternativePaymentViewModelSection
+
+    @Binding
+    private(set) var focusedInputId: AnyHashable?
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: POSpacing.small) {
+            if let title = section.title {
+                Text(title).textStyle(style.sectionTitle)
+            }
+            ForEach(section.items) { element in
+                NativeAlternativePaymentItemView(item: element, focusedInputId: $focusedInputId)
+            }
+            if let error = section.error {
+                Text(error).textStyle(style.errorDescription)
+            }
+        }
+        .backport.geometryGroup()
+    }
+
+    // MARK: - Private Properties
+
+    @Environment(\.nativeAlternativePaymentStyle)
+    private var style
+}
