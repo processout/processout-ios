08dd4dd524344fc3f840c6aeb5fd7e730671d437
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2023-12-05 14:53:42 +0100

    Migrate retry decorator

--- Sources/ProcessOut/Sources/Connectors/Http/Implementations/Retry/HttpConnectorRetryDecorator.swift
+++ Sources/ProcessOut/Sources/Connectors/Http/Implementations/Retry/HttpConnectorRetryDecorator.swift
@@ -14,18 +14,50 @@ final class HttpConnectorRetryDecorator: HttpConnector {
         self.retryStrategy = retryStrategy
     }
 
-    func execute<Value>(
-        request: HttpConnectorRequest<Value>, completion: @escaping (Result<Value, HttpConnectorFailure>) -> Void
-    ) -> POCancellable {
-        let operation = HttpConnectorRetryDecoratorOperation(
-            connector: connector, retryStrategy: retryStrategy, request: request, completion: completion
-        )
-        operation.start()
-        return operation
+    func execute<Value>(request: HttpConnectorRequest<Value>) async throws -> Value {
+        do {
+            return try await connector.execute(request: request)
+        } catch {
+            return try await retry(request: request, error: error, attempt: 0)
+        }
     }
 
     // MARK: - Private Properties
 
     private let connector: HttpConnector
     private let retryStrategy: RetryStrategy
+
+    // MARK: - Private Methods
+
+    private func retry<Value>(request: HttpConnectorRequest<Value>, error: Error, attempt: Int) async throws -> Value {
+        guard shouldRetry(after: error), attempt < retryStrategy.maximumRetries else {
+            throw error
+        }
+        do {
+            let delay = retryStrategy.interval(for: attempt)
+            try await Task.sleep(nanoseconds: UInt64(delay * 1e9))
+        } catch {
+            throw Failure.cancelled
+        }
+        do {
+            return try await connector.execute(request: request)
+        } catch {
+            return try await retry(request: request, error: error, attempt: attempt + 1)
+        }
+    }
+
+    private func shouldRetry(after error: Error) -> Bool {
+        guard let failure = error as? Failure else {
+            assertionFailure("Unexpected error type \(error).")
+            return false
+        }
+        switch failure {
+        case .networkUnreachable, .timeout:
+            return true
+        case .server(_, let statusCode):
+            return (500...599).contains(statusCode)
+        default:
+            return false
+        }
+    }
 }
