a16b4d9be41cd0ddf34c530210e5eecc6296efae
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-05 13:04:34 +0100

    Remove CKO target test workaround

--- Scripts/Test.sh
+++ Scripts/Test.sh
@@ -6,7 +6,7 @@ PROJECT='ProcessOut.xcodeproj'
 DESTINATION=$(./Scripts/TestDestination.swift)
 
 # Run Tests
-for PRODUCT in "ProcessOut" "ProcessOutUI"; do
+for PRODUCT in "ProcessOut" "ProcessOutUI" "ProcessOutCheckout3DS"; do
     xcodebuild clean test \
         -destination "$DESTINATION" \
         -project $PROJECT \
@@ -14,13 +14,5 @@ for PRODUCT in "ProcessOut" "ProcessOutUI"; do
         -enableCodeCoverage NO |
         xcpretty
 done
-
-# It is a known issue that Checkout3DS v3.2.1 (framework that ProcessOutCheckout3DS
-# depends on) can't be properly tested without host application due to bug.
-xcodebuild clean build \
-    -project $PROJECT \
-    -scheme ProcessOutCheckout3DS \
-    -destination "generic/platform=iOS" |
-    xcpretty
 
 # todo(andrii-vysotskyi): run example target tests when POM-144 is resolved
