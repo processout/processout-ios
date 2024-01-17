6022d8620925d3c9b46f229f6f88335eb2158612
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-17 15:57:35 +0100

    Add section view

---
+++ Sources/ProcessOutUI/Sources/Modules/CardUpdate/View/CardUpdateSectionView.swift
@@ -0,0 +1,32 @@
+//
+//  CardUpdateSectionView.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 17.01.2024.
+//
+
+import SwiftUI
+@_spi(PO) import ProcessOutCoreUI
+
+@available(iOS 14, *)
+struct CardUpdateSectionView: View {
+
+    let section: CardUpdateViewModelSection
+
+    @Binding
+    private(set) var focusedInputId: AnyHashable?
+
+    var body: some View {
+        VStack(alignment: .leading, spacing: POSpacing.small) {
+            if let title = section.title {
+                // todo(andrii-vysotskyi): allow style change when related functionality is public
+                let textStyle = POTextStyle(color: Color(poResource: .Text.secondary), typography: .Fixed.labelHeading)
+                Text(title).textStyle(textStyle)
+            }
+            ForEach(section.items) { element in
+                CardUpdateItemView(item: element, focusedInputId: $focusedInputId)
+            }
+        }
+        .backport.geometryGroup()
+    }
+}
---
+++ Sources/ProcessOutUI/Sources/Modules/CardUpdate/ViewModel/CardUpdateViewModelSection.swift
@@ -0,0 +1,20 @@
+//
+//  CardUpdateViewModelSection.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 17.01.2024.
+//
+
+import Foundation
+
+struct CardUpdateViewModelSection: Identifiable {
+
+    /// Section id.
+    let id: AnyHashable
+
+    /// Section title if any.
+    let title: String?
+
+    /// Section items.
+    let items: [CardUpdateViewModelItem]
+}
