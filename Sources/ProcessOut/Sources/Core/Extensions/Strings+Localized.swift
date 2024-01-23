03d8c491df64f9607b4d1248d3b27f4651e3b9d5
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-23 16:19:01 +0100

    Remove unused files

--- Sources/ProcessOut/Sources/Core/Extensions/Strings+Localized.swift
+++ Sources/ProcessOut/Sources/Core/Extensions/Strings+Localized.swift
@@ -7,31 +7,7 @@
 
 import Foundation
 
-extension Strings {
-
-    /// The implementation attempts to return translation in a language that is equal to a first entry in
-    /// the main bundle's `preferredLocalizations` array.
-    ///
-    /// It also prevents languages mismatch. For example, when the main application has both German
-    /// and English as preferred languages, its interface is going to be in German. But if we support only
-    /// English, our bundle would use it as a preferred language and render strings in it.
-    ///
-    /// If the main application uses a language that we don't support, the implementation would attempt
-    /// to use strings from the main bundle so that users can add missing localizations without having to
-    /// modify the SDK.
-    ///
-    /// The implementation falls back to the framework's bundle in case the main application doesn't
-    /// provide translations for missing languages to ensure that we don't display untranslated strings.
-    static func localized(_ key: String, _ table: String, _: String) -> String {
-        if Bundle.main.preferredLocalizations.first == BundleLocator.bundle.preferredLocalizations.first {
-            return BundleLocator.bundle.localizedString(forKey: key, value: nil, table: table)
-        }
-        let string = Bundle.main.localizedString(forKey: key, value: Constants.unknownString, table: table)
-        if string != Constants.unknownString {
-            return string
-        }
-        return BundleLocator.bundle.localizedString(forKey: key, value: nil, table: table)
-    }
+enum Strings {
 
     static var preferredLocalization: String {
         let bundle: Bundle
@@ -48,18 +24,11 @@ extension Strings {
         return bundle.preferredLocalizations.first!
     }
 
-    // MARK: - Private Nested Types
-
-    private enum Constants {
-        static let unknownString = UUID().uuidString
-        static let stringTable = "ProcessOut"
-    }
-
     // MARK: - Private Methods
 
     private static func hasStringsPreferredLocalization(ofType type: String, in bundle: Bundle) -> Bool {
         let path = bundle.path(
-            forResource: Constants.stringTable,
+            forResource: "ProcessOut",
             ofType: type,
             inDirectory: nil,
             forLocalization: bundle.preferredLocalizations.first
