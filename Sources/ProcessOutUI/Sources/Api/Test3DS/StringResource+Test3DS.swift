72272850f1f6806f81e7d3a5508f4777d084a39f
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-23 12:15:28 +0100

    Update Test3DS service resources

--- Sources/ProcessOutUI/Sources/Api/Test3DS/StringResource+Test3DS.swift
+++ Sources/ProcessOutUI/Sources/Api/Test3DS/StringResource+Test3DS.swift
@@ -5,17 +5,19 @@
 //  Created by Andrii Vysotskyi on 21.11.2023.
 //
 
-extension StringResource {
+@_spi(PO) import ProcessOut
+
+extension POStringResource {
 
     enum Test3DS {
 
         /// 3DS challenge title.
-        static let title = StringResource("test-3ds.challenge.title", comment: "")
+        static let title = POStringResource("test-3ds.challenge.title", comment: "")
 
         /// Accept button title.
-        static let accept = StringResource("test-3ds.challenge.accept", comment: "")
+        static let accept = POStringResource("test-3ds.challenge.accept", comment: "")
 
         /// Reject button title.
-        static let reject = StringResource("test-3ds.challenge.reject", comment: "")
+        static let reject = POStringResource("test-3ds.challenge.reject", comment: "")
     }
 }
