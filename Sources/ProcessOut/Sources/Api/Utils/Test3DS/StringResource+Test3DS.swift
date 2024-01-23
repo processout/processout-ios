a1139abe16e84cfa8c17bedf2a98274b869064c6
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-23 14:32:27 +0100

    Add test 3ds resources

---
+++ Sources/ProcessOut/Sources/Api/Utils/Test3DS/StringResource+Test3DS.swift
@@ -0,0 +1,21 @@
+//
+//  StringResource+Test3DS.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 23.01.2024.
+//
+
+extension POStringResource {
+
+    enum Test3DS {
+
+        /// 3DS challenge title.
+        static let title = POStringResource("test-3-d-s.challenge.title", comment: "")
+
+        /// Accept button title.
+        static let accept = POStringResource("test-3-d-s.challenge.accept", comment: "")
+
+        /// Reject button title.
+        static let reject = POStringResource("test-3-d-s.challenge.reject", comment: "")
+    }
+}
