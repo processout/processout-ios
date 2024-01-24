d392876ada99498b0a0f5e3bb113bc177ae561f2
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-01-24 17:03:51 +0100

    Fix localization update script

--- Scripts/UpdateLocalizations.sh
+++ Scripts/UpdateLocalizations.sh
@@ -2,22 +2,36 @@
 
 set -euo pipefail
 
-# Define variables
+# Constants
 WORK_DIR=$(mktemp -d)
+BASE_LANGUAGE=en
+BASE_LANGUAGE_XLIFF="$WORK_DIR/export/$BASE_LANGUAGE.xcloc/Localized Contents/$BASE_LANGUAGE.xliff"
 
 # Load env
 export $(cat .env | xargs)
 
-# Downloads strings
+# Create directories
+mkdir -p $WORK_DIR/import
+mkdir -p $WORK_DIR/export
+
+# Export base localization
+xcodebuild -exportLocalizations \
+  -project ProcessOut.xcodeproj \
+  -localizationPath $WORK_DIR/export \
+  -exportLanguage $BASE_LANGUAGE
+
+# Download strings
 lokalise2 \
     --token $LOKALISE_TOKEN \
     --project-id "20942964654390ceed1ab9.44453861" \
     file download \
     --format xliff \
-    --unzip-to $WORK_DIR
+    --unzip-to $WORK_DIR/import
 
-# Searches for all xliff files in a work directory and imports them in project
-find $WORK_DIR -name '*.xliff' -exec xcodebuild -importLocalizations -project ProcessOut.xcodeproj -localizationPath '{}' \;
+# Preprocess imported XLIFFs and import them in project
+find $WORK_DIR/import -name '*.xliff' \
+  -exec ./Scripts/PreprocessLocalization.swift "$BASE_LANGUAGE_XLIFF" '{}' \; \
+  -exec xcodebuild -importLocalizations -project ProcessOut.xcodeproj -localizationPath '{}' \;
 
 function cleanup {
   rm -rf $WORK_DIR
