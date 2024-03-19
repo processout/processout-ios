099e2cde1cd85534a97d1109f280d65b12edaff4
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-03-19 16:10:22 +0100

    Add card configuration

---
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Configuration/PODynamicCheckoutCardConfiguration.swift
@@ -0,0 +1,24 @@
+//
+//  PODynamicCheckoutCardConfiguration.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 27.02.2024.
+//
+
+public struct PODynamicCheckoutCardConfiguration {
+
+    /// Indicates if the input for entering the cardholder name should be displayed. Defaults to `true`.
+    public var isCardholderNameInputVisible = true
+
+    /// Primary action text, such as "Submit".
+    public var primaryActionTitle: String?
+
+    /// Primary action text, such as "Cancel". Use empty string to hide button.
+    public var cancelActionTitle: String?
+
+    /// Card billing address collection configuration.
+    public var billingAddress = POBillingAddressConfiguration()
+
+    /// Metada related to the card.
+    public var metadata: [String: String]?
+}
