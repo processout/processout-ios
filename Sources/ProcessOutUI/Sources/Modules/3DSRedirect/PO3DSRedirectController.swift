ec58cf6bee5d36b0b76ed987aac2d27f1d70691a
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2024-03-19 11:37:29 +0100

    feat(ad-hoc): add APM controller (#248)
    * Replace Safari delegate with completions to simplify code
    * Add APM controller to allow presentation when UIKit
    context is not available (will be used with Dynamic Checkout)

--- Sources/ProcessOutUI/Sources/Modules/3DSRedirect/PO3DSRedirectController.swift
+++ Sources/ProcessOutUI/Sources/Modules/3DSRedirect/PO3DSRedirectController.swift
@@ -67,6 +67,8 @@ public final class PO3DSRedirectController {
 
     /// Dismisses the Redirect UI.
     public func dismiss(completion: (() -> Void)? = nil) {
+        // todo(andrii-vysotskyi): automatically dismiss controller so behaviour
+        // matches `POAlternativePaymentMethodController`.
         if let safariViewController, safariViewController.presentingViewController != nil {
             self.safariViewController = nil
             safariViewController.dismiss(animated: true, completion: completion)
