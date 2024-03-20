9a105f134e0daff1983afd66727dd4bf512c623e
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-03-20 13:46:50 +0100

    Update delegate

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Delegate/PODynamicCheckoutDelegate.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Delegate/PODynamicCheckoutDelegate.swift
@@ -10,5 +10,25 @@ import ProcessOut
 public protocol PODynamicCheckoutDelegate: AnyObject {
 
     /// Invoked when module emits dynamic checkout event.
+    /// - NOTE: default implementation does nothing.
     func dynamicCheckout(didEmitEvent event: PODynamicCheckoutEvent)
+
+    /// Called when dynamic checkout is about to authorize invoice with given request.
+    ///
+    /// Your implementation may alter request parameters and return new request but make
+    /// sure that invoice id and source stay the same.
+    ///
+    /// - NOTE: default implementation returns unmodified request as is.
+    func dynamicCheckout(willAuthorizeInvoiceWith request: inout POInvoiceAuthorizationRequest) async
+}
+
+extension PODynamicCheckoutDelegate {
+
+    public func dynamicCheckout(didEmitEvent event: PODynamicCheckoutEvent) {
+        // Ignored
+    }
+
+    public func dynamicCheckout(willAuthorizeInvoiceWith request: inout POInvoiceAuthorizationRequest) async {
+        // Ignored
+    }
 }
