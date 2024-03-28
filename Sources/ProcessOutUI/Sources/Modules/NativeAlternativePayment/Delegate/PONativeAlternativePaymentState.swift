a0da95b41dbcf36e80319086009853353bf6dd3c
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-03-28 12:25:43 +0100

    Add nAPM state

---
+++ Sources/ProcessOutUI/Sources/Modules/NativeAlternativePayment/Delegate/PONativeAlternativePaymentState.swift
@@ -0,0 +1,44 @@
+//
+//  PONativeAlternativePaymentState.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 28.03.2024.
+//
+
+import ProcessOut
+
+public enum PONativeAlternativePaymentState {
+
+    public struct Started {
+
+        /// Indicates whether form in its current state could be submitted.
+        public let isSubmittable: Bool
+
+        /// Boolean value indicating whether cancel is supported in the current state.
+        public let isCancellable: Bool
+    }
+
+    public struct AwaitingCapture {
+
+        /// Boolean value indicating whether cancel is supported in the current state.
+        public let isCancellable: Bool
+    }
+
+    /// Initial interactor state.
+    case idle
+
+    /// Payment is being loaded.
+    case starting
+
+    /// Payment has started.
+    case started(Started)
+
+    /// Parameter values are being submitted.
+    case submitting
+
+    /// Parameters were submitted and accepted.
+    case awaitingCapture(AwaitingCapture)
+
+    /// Payment is completed.
+    case completed(result: Result<Void, POFailure>)
+}
