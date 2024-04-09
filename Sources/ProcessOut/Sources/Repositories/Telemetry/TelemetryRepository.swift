d863dc7a4d26c90f10e8564b91eda2d852203725
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-04-09 13:16:05 +0200

    Add base repository conformance

--- Sources/ProcessOut/Sources/Repositories/Telemetry/TelemetryRepository.swift
+++ Sources/ProcessOut/Sources/Repositories/Telemetry/TelemetryRepository.swift
@@ -5,9 +5,7 @@
 //  Created by Andrii Vysotskyi on 08.04.2024.
 //
 
-import Foundation
-
-protocol TelemetryRepository {
+protocol TelemetryRepository: PORepository {
 
     /// Submits telemetery.
     func submit(telemetry: Telemetry) async throws
