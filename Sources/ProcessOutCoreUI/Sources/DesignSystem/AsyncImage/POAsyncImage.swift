4271d4c26d21b8e89ccb28e1a9be4032e955d242
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-04-19 19:10:06 +0200

    Add async image view

---
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/AsyncImage/POAsyncImage.swift
@@ -0,0 +1,58 @@
+//
+//  POAsyncImage.swift
+//  ProcessOutCoreUI
+//
+//  Created by Andrii Vysotskyi on 19.04.2024.
+//
+
+import SwiftUI
+
+@_spi(PO)
+@MainActor
+@available(iOS 14, *)
+public struct POAsyncImage<Content: View>: View {
+
+    /// - Parameters:
+    ///   - content: A closure that takes the load phase as an input,
+    ///   and returns the view to display for the specified phase.
+    public init(
+        image: @Sendable @escaping () async throws -> Image?,
+        transaction: Transaction,
+        @ViewBuilder content: @escaping (POAsyncImagePhase) -> Content
+    ) {
+        self.image = image
+        self.transaction = transaction
+        self.content = content
+        phase = .empty
+    }
+
+    public var body: some View {
+        content(phase).backport.task(priority: .userInitiated, resolveImage)
+    }
+
+    // MARK: - Private Properties
+
+    private let image: @Sendable () async throws -> Image?
+    private let transaction: Transaction
+    private let content: (POAsyncImagePhase) -> Content
+
+    @State
+    private var phase: POAsyncImagePhase
+
+    // MARK: - Private Methods
+
+    /// Implementation resolves image and updates phase.
+    @Sendable
+    private func resolveImage() async {
+        let newPhase: POAsyncImagePhase
+        do {
+            guard let image = try await image() else {
+                return
+            }
+            newPhase = .success(image)
+        } catch {
+            newPhase = .failure(error)
+        }
+        withTransaction(transaction) { phase = newPhase }
+    }
+}
