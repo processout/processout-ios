81a0dd00bc2f91b86dbf5180e2926d7ea9894bba
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-04-23 16:10:30 +0200

    Improve style

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Style/PODynamicCheckoutStyle.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Style/PODynamicCheckoutStyle.swift
@@ -15,15 +15,39 @@ import SwiftUI
 @available(iOS 14, *)
 public struct PODynamicCheckoutStyle {
 
-    /// Payment item style.
-    public var payment = PODynamicCheckoutPaymentStyle()
+    public struct PaymentsSection {
 
-    /// Separator color.
-    public var separatorColor = Color(poResource: .Border.subtle)
+        /// Section title style if any.
+        public var title = POTextStyle(color: Color(poResource: .Text.primary), typography: .Medium.title)
+
+        /// Section border style.
+        public var border = POBorderStyle.regular(color: Color(poResource: .Text.muted))
+    }
+
+    public struct PaymentHeader {
+
+        /// Title style.
+        public var title = POTextStyle(color: Color(poResource: .Text.primary), typography: .Medium.title)
+
+        /// Information text style.
+        public var informationText = POTextStyle(color: Color(poResource: .Text.primary), typography: .Fixed.body)
+    }
+
+    /// Payments section style.
+    public var paymentsSection = PaymentsSection()
+
+    /// Style to apply to divider sepparating different sections.
+    public var sectionsDivider = POLabeledDividerStyle()
+
+    /// Payment item's header style.
+    public var paymentHeader = PaymentHeader()
 
     /// Background color.
     public var backgroundColor = Color(poResource: .Surface.level1)
 
     /// Actions container style.
     public var actionsContainer = POActionsContainerStyle.default
+
+    /// Creates style instance.
+    public init() { }
 }
