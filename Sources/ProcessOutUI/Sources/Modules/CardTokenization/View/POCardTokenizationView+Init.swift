e13f3a2ef6949bbe3a0b899da7da113de4f84f42
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-04-25 17:08:45 +0200

    Ensure card tokenization interactor / view model is created only once

--- Sources/ProcessOutUI/Sources/Modules/CardTokenization/View/POCardTokenizationView+Init.swift
+++ Sources/ProcessOutUI/Sources/Modules/CardTokenization/View/POCardTokenizationView+Init.swift
@@ -11,21 +11,28 @@
 extension POCardTokenizationView {
 
     /// Creates card tokenization view.
+    ///
     /// - Parameters:
     ///   - completion: Completion to invoke when flow is completed.
+    ///
+    /// - NOTE: Use caution when using this view, because SwiftUI only initializes
+    /// its state once during the lifetime of the view — even if you call the initializer
+    /// more than once — which might result in unexpected behavior.
     public init(
         configuration: POCardTokenizationConfiguration = .init(),
         delegate: POCardTokenizationDelegate? = nil,
         completion: @escaping (Result<POCard, POFailure>) -> Void
     ) {
-        let interactor = DefaultCardTokenizationInteractor(
-            cardsService: ProcessOut.shared.cards,
-            logger: ProcessOut.shared.logger,
-            configuration: configuration,
-            delegate: delegate,
-            completion: completion
-        )
-        let viewModel = DefaultCardTokenizationViewModel(interactor: interactor)
-        self = .init(viewModel: viewModel)
+        let viewModel = {
+            let interactor = DefaultCardTokenizationInteractor(
+                cardsService: ProcessOut.shared.cards,
+                logger: ProcessOut.shared.logger,
+                configuration: configuration,
+                delegate: delegate,
+                completion: completion
+            )
+            return DefaultCardTokenizationViewModel(interactor: interactor)
+        }
+        self = .init(viewModel: viewModel())
     }
 }
--- Sources/ProcessOutUI/Sources/Modules/CardTokenization/View/POCardTokenizationView.swift
+++ Sources/ProcessOutUI/Sources/Modules/CardTokenization/View/POCardTokenizationView.swift
@@ -12,8 +12,8 @@ import SwiftUI
 @available(iOS 14, *)
 public struct POCardTokenizationView: View {
 
-    init(viewModel: some CardTokenizationViewModel) {
-        self._viewModel = .init(wrappedValue: .init(erasing: viewModel))
+    init(viewModel: @autoclosure @escaping () -> some CardTokenizationViewModel) {
+        self._viewModel = .init(wrappedValue: .init(erasing: viewModel()))
     }
 
     // MARK: - View
