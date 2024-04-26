b4d544fcfe00fd808409a4dd3393cb2bb9c225dc
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-04-26 12:38:05 +0200

    Add scroll view proxy environment

---
+++ Sources/ProcessOutCoreUI/Sources/Core/Modifiers/ScrollViewProxy/View+ScrollViewProxy.swift
@@ -0,0 +1,34 @@
+//
+//  View+ScrollViewProxy.swift
+//  ProcessOutCoreUI
+//
+//  Created by Andrii Vysotskyi on 26.04.2024.
+//
+
+import SwiftUI
+
+extension View {
+
+    /// Sets the style for card tokenization views within this view.
+    @_spi(PO)
+    @available(iOS 14, *)
+    public func scrollViewProxy(_ scrollView: ScrollViewProxy?) -> some View {
+        environment(\.scrollViewProxy, scrollView)
+    }
+}
+
+@available(iOS 14, *)
+extension EnvironmentValues {
+
+    @_spi(PO)
+    public var scrollViewProxy: ScrollViewProxy? {
+        get { self[Key.self] }
+        set { self[Key.self] = newValue }
+    }
+
+    // MARK: - Private Nested Types
+
+    private struct Key: EnvironmentKey {
+        static let defaultValue: ScrollViewProxy? = nil
+    }
+}
