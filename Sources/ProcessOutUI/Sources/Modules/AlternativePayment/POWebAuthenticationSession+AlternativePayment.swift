ebecb2bdd50e005b78d6a53cac9191615ad46e92
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-05-29 14:41:59 +0200

    Migrate alternative payment

---
+++ Sources/ProcessOutUI/Sources/Modules/AlternativePayment/POWebAuthenticationSession+AlternativePayment.swift
@@ -0,0 +1,58 @@
+//
+//  POWebAuthenticationSession+AlternativePayment.swift
+//  ProcessOutUI
+//
+//  Created by Andrii Vysotskyi on 18.03.2024.
+//
+
+import Foundation
+import ProcessOut
+
+extension POWebAuthenticationSession {
+
+    public typealias AlternativePaymentCompletion = (Result<POAlternativePaymentMethodResponse, POFailure>) -> Void
+
+    /// Creates session that is capable of handling alternative payment.
+    /// 
+    /// - Parameters:
+    ///   - request: Alternative payment request.
+    ///   - returnUrl: Return URL specified when creating invoice.
+    ///   - completion: Completion to invoke when APM flow completes.
+    public convenience init(
+        request: POAlternativePaymentMethodRequest,
+        returnUrl: URL,
+        completion: @escaping AlternativePaymentCompletion
+    ) {
+        let url = ProcessOut.shared.alternativePaymentMethods.alternativePaymentMethodUrl(request: request)
+        self.init(url: url, returnUrl: returnUrl, completion: completion)
+    }
+
+    /// Creates session that is capable of handling alternative payment.
+    ///
+    /// - Parameters:
+    ///   - url: initial URL instead of **request**. Implementation does not validate
+    ///   whether given value is valid to actually start APM flow.
+    ///   - returnUrl: Return URL specified when creating invoice.
+    ///   - completion: Completion to invoke when APM flow completes.
+    public convenience init(url: URL, returnUrl: URL, completion: @escaping AlternativePaymentCompletion) {
+        let completionBox: Completion = { result in
+            completion(result.flatMap(Self.response(with:)))
+        }
+        self.init(url: url, callback: .customScheme(returnUrl.scheme ?? ""), completion: completionBox)
+    }
+
+    // MARK: - Private Methods
+
+    private static func response(with url: URL) -> Result<POAlternativePaymentMethodResponse, POFailure> {
+        do {
+            let response = try ProcessOut.shared.alternativePaymentMethods.alternativePaymentMethodResponse(url: url)
+            return .success(response)
+        } catch let failure as POFailure {
+            return .failure(failure)
+        } catch {
+            assertionFailure("Expected POFailure instance.")
+            let failure = POFailure(code: .generic(.mobile))
+            return .failure(failure)
+        }
+    }
+}
