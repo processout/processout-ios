07ee2a29580444eca254938ee7ee7fbc3bdd7761
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-05-29 12:34:41 +0200

    Fix typo

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/View/PODynamicCheckoutView+Init.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/View/PODynamicCheckoutView+Init.swift
@@ -14,7 +14,7 @@ extension PODynamicCheckoutView {
     ///
     /// - NOTE: Use caution when using this view, because SwiftUI only initializes
     /// its state once during the lifetime of the view — even if you call the initializer
-    /// more than once — which might result in unexpected behavior.
+    /// more than once — which might result in unexpected behaviour.
     public init(
         configuration: PODynamicCheckoutConfiguration,
         delegate: PODynamicCheckoutDelegate,
