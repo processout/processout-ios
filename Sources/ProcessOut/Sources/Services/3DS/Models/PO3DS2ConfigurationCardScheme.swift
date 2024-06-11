d66ab578397901bee425791317fdc6bd4aeb6335
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-06-11 15:55:50 +0200

    Return old type for backward compatibility

---
+++ Sources/ProcessOut/Sources/Services/3DS/Models/PO3DS2ConfigurationCardScheme.swift
@@ -0,0 +1,12 @@
+//
+//  PO3DS2ConfigurationCardScheme.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 27.03.2023.
+//
+
+import Foundation
+
+/// Available card schemes.
+@available(*, deprecated, message: "Use POCardScheme directly.")
+public typealias PO3DS2ConfigurationCardScheme = POCardScheme
