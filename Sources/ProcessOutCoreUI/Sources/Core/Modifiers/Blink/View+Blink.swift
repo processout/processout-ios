b3442fda4511befc7fd3665851c88af7dc152c11
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-06-17 12:41:14 +0200

    Add blink modifier

---
+++ Sources/ProcessOutCoreUI/Sources/Core/Modifiers/Blink/View+Blink.swift
@@ -0,0 +1,35 @@
+//
+//  BlinkViewModifier.swift
+//  ProcessOutCoreUI
+//
+//  Created by Andrii Vysotskyi on 16.06.2024.
+//
+
+import SwiftUI
+
+extension View {
+
+    func blink(animation: Animation = .default) -> some View {
+        modifier(BlinkViewModifier(animation: animation))
+    }
+}
+
+private struct BlinkViewModifier: ViewModifier {
+
+    let animation: Animation
+
+    // MARK: - ViewModifier
+
+    func body(content: Content) -> some View {
+        content
+            .opacity(isVisible ? 1 : 0.1)
+            .onAppear {
+                isVisible = false
+            }
+            .animation(animation.repeatForever(), value: isVisible)
+    }
+
+    // MARK: - Private Properties
+
+    @State private var isVisible = true
+}
