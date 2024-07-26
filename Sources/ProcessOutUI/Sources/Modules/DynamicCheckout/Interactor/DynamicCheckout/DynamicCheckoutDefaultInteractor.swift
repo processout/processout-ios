2d545f690f43076e4c149829892d5526ffdfb54a
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-07-26 10:56:42 +0200

    Lower business errors log level to warn

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Interactor/DynamicCheckout/DynamicCheckoutDefaultInteractor.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Interactor/DynamicCheckout/DynamicCheckoutDefaultInteractor.swift
@@ -647,7 +647,7 @@ final class DynamicCheckoutDefaultInteractor:
             logger.debug("Unexpected error type: \(error)")
             failure = POFailure(code: .generic(.mobile), underlyingError: error)
         }
-        logger.error("Did fail to process dynamic checkout payment: '\(error)'")
+        logger.warn("Did fail to process dynamic checkout payment: '\(error)'")
         state = .failure(failure)
         send(event: .didFail(failure: failure))
         completion(.failure(failure))
