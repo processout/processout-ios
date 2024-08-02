65c8d4234b6f6f7220fe60078fca0640ce4989b9
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-02 13:43:45 +0200

    Commit missing file

---
+++ Sources/ProcessOut/Sources/Services/3DS/Models/PO3DS2ChallengeResult.swift
@@ -0,0 +1,23 @@
+//
+//  PO3DS2ChallengeResult.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 01.08.2024.
+//
+
+/// Contains information about completion of the challenge process.
+public struct PO3DS2ChallengeResult: Encodable, Sendable {
+
+    /// The transaction status that was received in the final challenge response.
+    public let transactionStatus: String
+
+    public init(transactionStatus: String) {
+        self.transactionStatus = transactionStatus
+    }
+
+    // MARK: - Private Nested Types
+
+    private enum CodingKeys: String, CodingKey {
+        case transactionStatus = "transStatus"
+    }
+}
