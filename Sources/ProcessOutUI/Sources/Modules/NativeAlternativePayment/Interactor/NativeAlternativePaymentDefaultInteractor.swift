de7d609f4e6d1a7649f2bc43fb94b8b33e3ac7c9
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-13 13:03:15 +0200

    Lower precondition to assert

--- Sources/ProcessOutUI/Sources/Modules/NativeAlternativePayment/Interactor/NativeAlternativePaymentDefaultInteractor.swift
+++ Sources/ProcessOutUI/Sources/Modules/NativeAlternativePayment/Interactor/NativeAlternativePaymentDefaultInteractor.swift
@@ -513,7 +513,7 @@ final class NativeAlternativePaymentDefaultInteractor:
                 switch parameter.specification.type {
                 case .singleSelect:
                     let availableValues = parameter.specification.availableValues?.map(\.value) ?? []
-                    precondition(availableValues.contains(value), "Unknown `singleSelect` parameter value.")
+                    assert(availableValues.contains(value), "Unknown `singleSelect` parameter value.")
                     defaultValue = value
                 default:
                     defaultValue = parameter.formatter?.string(for: value) ?? value
