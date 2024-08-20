4b38581fb5a9773976459c112cec65cea5b24092
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-20 14:33:18 +0200

    Add constants in code

---
+++ Tests/ProcessOutUITests/Sources/Core/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//
+//  Created by Andrii Vysotskyi on 20.08.2024.
+//
+
+enum Constants {
+
+    /// Project ID.
+    static let projectId = ""
+
+    /// Project private key.
+    static let projectPrivateKey = ""
+
+    /// Customer ID.
+    static let customerId = ""
+}
