5074acf0e117ed575a083cc855c55f0d5d0fdaad
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-27 16:13:35 +0200

    Authorize regular APMs

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Interactor/DynamicCheckout/DynamicCheckoutDefaultInteractor.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Interactor/DynamicCheckout/DynamicCheckoutDefaultInteractor.swift
@@ -446,7 +446,12 @@ final class DynamicCheckoutDefaultInteractor:
         state = .paymentProcessing(paymentProcessingState)
         Task { @MainActor in
             do {
-                _ = try await alternativePaymentSession.start(url: method.configuration.redirectUrl)
+                let response = try await alternativePaymentSession.start(url: method.configuration.redirectUrl)
+                try await authorizeInvoice(
+                    source: response.gatewayToken,
+                    saveSource: false,
+                    startedState: startedState
+                )
                 setSuccessState()
             } catch {
                 recoverPaymentProcessing(error: error)
@@ -537,20 +542,11 @@ final class DynamicCheckoutDefaultInteractor:
         state = .paymentProcessing(paymentProcessingState)
         Task { @MainActor in
             do {
+                var source = method.configuration.customerTokenId
                 if let redirectUrl = method.configuration.redirectUrl {
-                    let response = try await alternativePaymentSession.start(url: redirectUrl)
-                    try await authorizeInvoice(
-                        source: response.gatewayToken,
-                        saveSource: false,
-                        startedState: startedState
-                    )
-                } else {
-                    try await authorizeInvoice(
-                        source: method.configuration.customerTokenId,
-                        saveSource: false,
-                        startedState: startedState
-                    )
+                    source = try await alternativePaymentSession.start(url: redirectUrl).gatewayToken
                 }
+                try await authorizeInvoice(source: source, saveSource: false, startedState: startedState)
                 setSuccessState()
             } catch {
                 recoverPaymentProcessing(error: error)
