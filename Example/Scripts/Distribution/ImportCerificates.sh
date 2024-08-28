576fb73a1bf13d6d8335e0a7360cb4cf2d47fba2
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-28 15:25:37 +0200

    Ensure keychain remains

--- Example/Scripts/Distribution/ImportCerificates.sh
+++ Example/Scripts/Distribution/ImportCerificates.sh
@@ -2,13 +2,6 @@
 
 set -euo pipefail
 
-function cleanup {
-  rm -rf $WORK_DIR
-}
-
-# Configure cleanup
-trap cleanup EXIT
-
 # Create temp directory
 WORK_DIR=$(mktemp -d)
 
@@ -17,7 +10,7 @@ DEVELOPMENT_CERTIFICATE_PATH="$WORK_DIR/DevelopmentCertificate.p12"
 echo -n $APPLE_DEVELOPMENT_CERTIFICATE_CONTENT | base64 -d -o $DEVELOPMENT_CERTIFICATE_PATH
 
 # Keychain constants
-KEYCHAIN_PATH="$WORK_DIR/signing.keychain-db"
+KEYCHAIN_PATH="$WORK_DIR/processout-signing.keychain-db"
 KEYCHAIN_PASSWORD=$(openssl rand -base64 32)
 
 # Create temporary keychain
@@ -29,12 +22,12 @@ security unlock-keychain -p $KEYCHAIN_PASSWORD $KEYCHAIN_PATH
 security import $DEVELOPMENT_CERTIFICATE_PATH \
     -P $APPLE_DEVELOPMENT_CERTIFICATE_PASSWORD \
     -k $KEYCHAIN_PATH \
-    -A \
-    -t cert \
-    -f pkcs12
+    -f pkcs12 \
+    -A
 
 # Enable codesigning from a non user interactive shell
-security set-key-partition-list -S apple-tool:,apple: -k $KEYCHAIN_PASSWORD $KEYCHAIN_PATH
+# security set-key-partition-list -S "apple-tool:,apple:" -s -k $KEYCHAIN_PASSWORD $KEYCHAIN_PATH
 
-# Append keychain to the user domain
-security list-keychain -d user -s $KEYCHAIN_PATH
+# Append temp keychain to the user domain
+EXISTING_USER_KEYCHAIN_PATHS=$(security list-keychain -d user | awk -F'"' '{print $2}' ORS=' ')
+security list-keychains -d user -s $KEYCHAIN_PATH $EXISTING_USER_KEYCHAIN_PATHS
