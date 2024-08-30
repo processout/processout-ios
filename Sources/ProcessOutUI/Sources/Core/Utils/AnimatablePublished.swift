2da18a25df93bfacd76ca95dccd4107fff996a3f
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-08-30 13:03:08 +0200

    Support disabling animations

--- Sources/ProcessOutUI/Sources/Core/Utils/AnimatablePublished.swift
+++ Sources/ProcessOutUI/Sources/Core/Utils/AnimatablePublished.swift
@@ -17,12 +17,24 @@ struct AnimatablePublished<T> where T: AnimationIdentityProvider {
     init(wrappedValue: T, animation: Animation = .default) {
         self.value = wrappedValue
         self.animation = animation
+        storage = Storage()
     }
 
     @available(*, unavailable, message: "@AnimatablePublished is only available on properties of classes")
     var wrappedValue: T {
         get { fatalError() }
         set { fatalError() }
+    }
+
+    var projectedValue: Self {
+        self
+    }
+
+    /// Disables animations for mutations performed within given closure.
+    func performWithoutAnimation(_ actionsWithoutAnimation: () -> Void) {
+        storage.areAnimationsEnabled = false
+        actionsWithoutAnimation()
+        storage.areAnimationsEnabled = true
     }
 
     static subscript<Instance: ObservableObject>(
@@ -37,7 +49,9 @@ struct AnimatablePublished<T> where T: AnimationIdentityProvider {
             // Changes that may affect layout are animated explicitly to ensure whole view hierarchy
             // is animated properly. Using `withAnimation` inside view model is not perfect (ideally
             // it should be done by view only) but is simpler.
-            let isAnimated = observed[keyPath: storageKeyPath].value.animationIdentity != newValue.animationIdentity
+            let areAnimationsEnabled = observed[keyPath: storageKeyPath].storage.areAnimationsEnabled
+            let oldAnimationIdentity = observed[keyPath: storageKeyPath].value.animationIdentity
+            let isAnimated = oldAnimationIdentity != newValue.animationIdentity && areAnimationsEnabled
             withAnimation(isAnimated ? observed[keyPath: storageKeyPath].animation : nil) {
                 observed.objectWillChange.send()
                 observed[keyPath: storageKeyPath].value = newValue
@@ -45,9 +59,20 @@ struct AnimatablePublished<T> where T: AnimationIdentityProvider {
         }
     }
 
+    // MARK: - Private Nested Types
+
+    private final class Storage {
+
+        /// Boolean value indicating whether animations are enabled.
+        var areAnimationsEnabled = true
+    }
+
     // MARK: - Private Properties
 
     private let animation: Animation
+    private let storage: Storage
+
+    /// Current value.
     private var value: T
 }
 
