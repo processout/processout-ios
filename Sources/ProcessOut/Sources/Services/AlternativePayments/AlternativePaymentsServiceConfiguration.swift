ceab040e575032fd1f8baded4a86844b08d73e38
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-09-11 12:50:10 +0200

    Backport alternative payments service

---
+++ Sources/ProcessOut/Sources/Services/AlternativePayments/AlternativePaymentsServiceConfiguration.swift
@@ -0,0 +1,17 @@
+//
+//  AlternativePaymentsServiceConfiguration.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 13.02.2024.
+//
+
+import Foundation
+
+struct AlternativePaymentsServiceConfiguration: Sendable {
+
+    /// Project ID.
+    let projectId: String
+
+    /// Base URL.
+    let baseUrl: URL
+}
