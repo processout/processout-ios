ca8e3c97b45c3e5a13ac24620d49c13df2086899
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-09-16 15:23:50 +0200

    Allow including private key with invoice request

--- Sources/ProcessOut/Sources/Repositories/Invoices/Requests/POInvoiceRequest.swift
+++ Sources/ProcessOut/Sources/Repositories/Invoices/Requests/POInvoiceRequest.swift
@@ -17,9 +17,23 @@ public struct POInvoiceRequest {
     /// included in the response if the invoice has an assigned customerID.
     public let clientSecret: String?
 
+    /// Boolean value indicating whether invoice should be requested with private
+    /// key attached to underlying request.
+    @_spi(PO)
+    public var attachPrivateKey: Bool
+
     /// Creates request instance.
     public init(invoiceId: String, clientSecret: String? = nil) {
         self.invoiceId = invoiceId
         self.clientSecret = clientSecret
+        self.attachPrivateKey = false
+    }
+
+    /// Creates request instance.
+    @_spi(PO)
+    public init(invoiceId: String, clientSecret: String? = nil, attachPrivateKey: Bool) {
+        self.invoiceId = invoiceId
+        self.clientSecret = clientSecret
+        self.attachPrivateKey = attachPrivateKey
     }
 }
