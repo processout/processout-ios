0ddafbe37f18b3e5c57cefcf1706d97d2e00e467
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-09-17 13:24:07 +0200

    Sign create cmark framework

--- Scripts/CMark/CreateXcframework.sh
+++ Scripts/CMark/CreateXcframework.sh
@@ -12,23 +12,20 @@ function cleanup {
 }
 
 # Configure cleanup
-# trap cleanup EXIT
+trap cleanup EXIT
 
+# Constants
 SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-OUTPUT_DIR="$SCRIPT_DIR/../../Vendor"
 WORK_DIR=$(mktemp -d)
 
 # Script expects revision as a first and only argument
 test $# -eq 1 || fail "Expected tag or branch reference."
 
-# Exports version as environment variable
-export CURRENT_VERSION=$1
-
 # Go to temporary directory
 cd $WORK_DIR
 
 # Clone library
-git clone --depth 1 --branch $CURRENT_VERSION https://github.com/swiftlang/swift-cmark .
+git clone --depth 1 --branch $1 https://github.com/swiftlang/swift-cmark .
 
 # Ensure modulemap represents darwin-style framework
 MODULEMAP_PATH="src/include/module.modulemap"
@@ -44,11 +41,32 @@ xcodegen generate
 xcodebuild archive -scheme cmark_gfm -destination "generic/platform=iOS" -archivePath ./cmark_gfm-iOS
 xcodebuild archive -scheme cmark_gfm -destination "generic/platform=iOS Simulator" -archivePath ./cmark_gfm-Sim
 
+# Define signing arguments
+CODESIGN_ARGS=(
+  --timestamp
+  --force
+  --sign "Apple Distribution: ProcessOut Inc. (3Y9WMX63ZJ)"
+  --preserve-metadata\=identifier,entitlements,flags
+)
+
+# Sign iOS framework
+/usr/bin/codesign "${CODESIGN_ARGS[@]}" ./cmark_gfm-iOS.xcarchive/Products/Library/Frameworks/cmark_gfm.framework
+
+# Sign iOS Simulator framework
+/usr/bin/codesign "${CODESIGN_ARGS[@]}" ./cmark_gfm-Sim.xcarchive/Products/Library/Frameworks/cmark_gfm.framework
+
+# Define output directory
+OUTPUT_DIR="$SCRIPT_DIR/../../Vendor"
+
 # Generate XCFramework
 xcodebuild -create-xcframework \
     -framework ./cmark_gfm-iOS.xcarchive/Products/Library/Frameworks/cmark_gfm.framework \
     -framework ./cmark_gfm-Sim.xcarchive/Products/Library/Frameworks/cmark_gfm.framework \
     -output "$OUTPUT_DIR/cmark_gfm.xcframework"
 
+# Sign XCFramework
+/usr/bin/codesign "${CODESIGN_ARGS[@]}" "$OUTPUT_DIR/cmark_gfm.xcframework"
+
 # Write metadata
-echo $CURRENT_VERSION > "$OUTPUT_DIR/cmark_gfm.xcframework.version"
+CURRENT_REVISION=$(git rev-parse --short HEAD)
+echo $CURRENT_REVISION > "$OUTPUT_DIR/cmark_gfm.xcframework.version"
