2005a7bbf88688cc96b2f0ea1f4c379732c20234
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-09-23 13:25:40 +0200

    Allow saving in example

--- Example/Example/Sources/UI/Modules/CardPayment/ViewModel/CardPaymentViewModel.swift
+++ Example/Example/Sources/UI/Modules/CardPayment/ViewModel/CardPaymentViewModel.swift
@@ -45,7 +45,9 @@ final class CardPaymentViewModel: ObservableObject {
     }
 
     private func setCardTokenizationItem() {
-        let configuration = POCardTokenizationConfiguration(isCardholderNameInputVisible: false)
+        let configuration = POCardTokenizationConfiguration(
+            isCardholderNameInputVisible: false, isSavingAllowed: true
+        )
         let cardTokenizationItem = CardPaymentViewModelState.CardTokenization(
             id: UUID().uuidString,
             configuration: configuration,
