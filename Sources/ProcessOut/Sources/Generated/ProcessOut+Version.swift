e581a8e7d1a75ac776f159cd905e23424d4388bb
ProcessOut CI <131873576+processout-ci@users.noreply.github.com>
2024-11-18 10:02:05 +0000

    feat(release): 4.23.0 (#392)
    Co-authored-by: andrii-vysotskyi-cko <andrii-vysotskyi-cko@users.noreply.github.com>

--- Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
+++ Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
@@ -7,8 +7,8 @@
 
 extension ProcessOut {
 
-    /// The current version of this library, value is "4.22.1".
+    /// The current version of this library, value is "4.23.0".
     public static var version: String {
-        "4.22.1"
+        "4.23.0"
     }
 }
