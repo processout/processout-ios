6a4bd0251da434c6e2dee47d24b15562c59dee28
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-11-20 13:18:25 +0100

    Remove test code

--- Sources/ProcessOut/Sources/Services/Invoices/DefaultInvoicesService.swift
+++ Sources/ProcessOut/Sources/Services/Invoices/DefaultInvoicesService.swift
@@ -74,7 +74,8 @@ final class DefaultInvoicesService: POInvoicesService {
     }
 
     func createInvoice(request: POInvoiceCreationRequest) async throws -> POInvoice {
-        try await repository.createInvoice(request: request)
+        try await Task.sleep(seconds: 5)
+        return try await repository.createInvoice(request: request)
     }
 
     // MARK: - Private Nested Types
