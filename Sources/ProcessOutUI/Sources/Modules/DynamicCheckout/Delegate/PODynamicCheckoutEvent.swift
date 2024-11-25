dc810dd27d05eba49bb6366900959ba73f694ad2
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2024-11-25 10:45:31 +0100

    feat(ad-hoc): add dynamic checkout events (#394)
    * Add new events
    * Ensure adding payment methods to PODynamicCheckoutPaymentMethod
    and PODynamicCheckoutEvent is backward compatible from API
    perspective.

--- Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Delegate/PODynamicCheckoutEvent.swift
+++ Sources/ProcessOutUI/Sources/Modules/DynamicCheckout/Delegate/PODynamicCheckoutEvent.swift
@@ -5,11 +5,21 @@
 //  Created by Andrii Vysotskyi on 29.02.2024.
 //
 
-import ProcessOut
+@_spi(PO) import ProcessOut
 
 /// Events emitted by dynamic checkout module during its lifecycle.
 @_spi(PO)
 public enum PODynamicCheckoutEvent: Sendable {
+
+    /// Payment method failure event details.
+    public struct DidFailPayment: Sendable {
+
+        /// Payment method that failed.
+        public let paymentMethod: PODynamicCheckoutPaymentMethod
+
+        /// Failure details.
+        public let failure: POFailure
+    }
 
     /// Initial event that is sent prior any other event.
     case willStart
@@ -19,11 +29,26 @@ public enum PODynamicCheckoutEvent: Sendable {
     case didStart
 
     /// Invoked when users requests selection of another payment method.
-    case willSelectPaymentMethod
+    case willSelectPaymentMethod(_ paymentMethod: PODynamicCheckoutPaymentMethod)
+
+    /// Payment method selection is not immediate, this event is sent when payment
+    /// method selection succeeds.
+    case didSelectPaymentMethod(_ paymentMethod: PODynamicCheckoutPaymentMethod)
+
+    /// Event is sent when certain payment method fails with retryable error.
+    case didFailPayment(DidFailPayment)
+
+    /// Event is sent when user asked to confirm cancellation.
+    case didRequestCancelConfirmation
 
     /// Event is sent after payment was confirmed to be captured. This is a final event.
     case didCompletePayment
 
     /// Event is sent in case unretryable error occurs. This is a final event.
     case didFail(failure: POFailure)
+
+    /// Placeholder to allow adding additional events while staying backward compatible.
+    /// - Warning: Don't match this case directly, instead use default.
+    @_spi(PO)
+    case unknown
 }
