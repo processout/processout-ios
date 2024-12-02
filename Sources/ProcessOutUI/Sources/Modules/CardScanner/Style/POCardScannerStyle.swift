8b996922140c25bbe4b5720643bdccd4ad1eb5d9
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-12-02 16:27:52 +0100

    Add style definition

---
+++ Sources/ProcessOutUI/Sources/Modules/CardScanner/Style/POCardScannerStyle.swift
@@ -0,0 +1,53 @@
+//
+//  POCardScannerStyle.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 02.12.2024.
+//
+
+import SwiftUI
+
+/// A type that specifies the appearance and interaction of all card scanners
+/// within a view hierarchy.
+@_spi(PO)
+@MainActor
+public protocol POCardScannerStyle: Sendable {
+
+    /// A view representing the appearance and interaction of a `POPicker`.
+    associatedtype Body: View
+
+    /// The properties of a card scanner.
+    typealias Configuration = POCardScannerStyleConfiguration
+
+    /// Returns the appearance and interaction content for a `POCardScannerView`.
+    ///
+    /// - Parameter configuration : The properties of the card scanner.
+    @ViewBuilder
+    func makeBody(configuration: Configuration) -> Self.Body
+}
+
+/// /// The properties of a card scanner.
+@_spi(PO)
+@MainActor
+public struct POCardScannerStyleConfiguration {
+
+    /// Card scanner title.
+    public let title: AnyView
+
+    /// Card scanner description.
+    public let description: AnyView
+
+    /// Video preview view.
+    public let videoPreview: AnyView
+
+    /// Creates configuration.
+    init(
+        @ViewBuilder title: () -> some View,
+        @ViewBuilder description: () -> some View,
+        @ViewBuilder videoPreview: () -> some View
+    ) {
+        self.title = AnyView(title())
+        self.description = AnyView(description())
+        self.videoPreview = AnyView(videoPreview())
+    }
+}
---
+++ Sources/ProcessOutUI/Sources/Modules/CardScanner/Style/View+CardScannerStyle.swift
@@ -0,0 +1,36 @@
+//
+//  View+CardScannerStyle.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 02.12.2024.
+//
+
+import SwiftUI
+
+@_spi(PO)
+extension View {
+
+    /// Sets the style for card scanner views within this view.
+    @available(iOS 14, *)
+    public func cardScannerStyle(_ style: any POCardScannerStyle) -> some View {
+        environment(\.cardScannerStyle, style)
+    }
+}
+
+@available(iOS 14, *)
+@_spi(PO)
+extension EnvironmentValues {
+
+    /// The size to apply to controls within a view.
+    ///
+    /// The default is ``POControlSize/regular``.
+    @MainActor
+    public var cardScannerStyle: any POCardScannerStyle {
+        get { self[Key.self] ?? .automatic }
+        set { self[Key.self] = newValue }
+    }
+
+    private struct Key: EnvironmentKey {
+        static let defaultValue: (any POCardScannerStyle)? = nil
+    }
+}
