1e0d3d1e9c6edb705f11e802286336b01ccc7da0
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-12-02 14:33:43 +0100

    Add control size backport

---
+++ Sources/ProcessOutCoreUI/Sources/Backports/ControlSize/View+ControlSize.swift
@@ -0,0 +1,54 @@
+//
+//  Backport+ControlSize.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 02.12.2024.
+//
+
+import SwiftUI
+
+extension POBackport where Wrapped: View {
+
+    /// Sets the size for controls within this view.
+    @available(iOS, deprecated: 15, message: "Use View/controlSize(_:) directly.")
+    @_spi(PO)
+    @ViewBuilder
+    public func poControlSize(_ controlSize: POControlSize) -> some View {
+        if #available(iOS 15, *) {
+            wrapped
+                .environment(\.poControlSize, controlSize)
+                .controlSize(.init(poControlSize: controlSize))
+        } else {
+            wrapped.environment(\.poControlSize, controlSize)
+        }
+    }
+}
+
+extension EnvironmentValues {
+
+    /// The size to apply to controls within a view.
+    ///
+    /// The default is ``POControlSize/regular``.
+    @available(iOS, deprecated: 17, message: "Use View/controlSize directly.")
+    public internal(set) var poControlSize: POControlSize {
+        get { self[Key.self] }
+        set { self[Key.self] = newValue }
+    }
+
+    private struct Key: EnvironmentKey {
+        static let defaultValue = POControlSize.regular
+    }
+}
+
+@available(iOS 15, *)
+extension ControlSize {
+
+    fileprivate init(poControlSize size: POControlSize) { // swiftlint:disable:this strict_fileprivate
+        switch size {
+        case .regular:
+            self = .regular
+        case .small:
+            self = .small
+        }
+    }
+}
