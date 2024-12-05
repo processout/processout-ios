31f1b4debc2eb7179403dbcc9620d1555b07fd85
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2024-12-05 14:35:39 +0100

    Add button toggle style

---
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/Toggle/Button/POButtonToggleStyle.swift
@@ -0,0 +1,40 @@
+//
+//  POButtonToggleStyle.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 04.12.2024.
+//
+
+import SwiftUI
+
+@available(iOS 14, *)
+public struct POButtonToggleStyle: ToggleStyle {
+
+    public init(buttonStyle: some ButtonStyle) {
+        self.buttonStyle = buttonStyle
+    }
+
+    // MARK: - ToggleStyle
+
+    public func makeBody(configuration: Configuration) -> some View {
+        Button {
+            configuration.$isOn.wrappedValue.toggle()
+        } label: {
+            configuration.label
+        }
+        .buttonStyle(.ghost)
+        .controlSelected(configuration.isOn)
+    }
+
+    // MARK: - Private Properties
+
+    private let buttonStyle: any ButtonStyle
+}
+
+@available(iOS 14, *)
+extension ToggleStyle where Self == POButtonToggleStyle {
+
+    public static var poButton: POButtonToggleStyle {
+        POButtonToggleStyle(buttonStyle: .ghost)
+    }
+}
