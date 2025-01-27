4fef78eafe0a19bf6a6032e096542615567c31b8
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-01-27 11:27:43 +0100

    Finalize input colors

--- Sources/ProcessOutCoreUI/Sources/DesignSystem/InputStyle/Color+Input.swift
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/InputStyle/Color+Input.swift
@@ -11,7 +11,22 @@ import SwiftUI
 
 extension Color {
 
-    enum Input {
+    @_spi(PO)
+    public enum Input {
+
+        @_spi(PO)
+        public enum Label {
+
+            /// The "Input/Label/Default" color.
+            public static let `default` = Color(
+                light: UIColor(0x000000), dark: UIColor(0xFFFFFF)
+            )
+
+            /// The "Input/Label/Error" color.
+            public static let error = Color(
+                light: UIColor(0xBE011B), dark: UIColor(0xFF8888)
+            )
+        }
 
         /// The "Input/Background" color namespace.
         enum Background {
@@ -19,12 +34,6 @@ extension Color {
             /// The "Input/Background/Default" color.
             static let `default` = Color(
                 light: UIColor(0x121314, alpha: 0.06), dark: UIColor(0xF6F8FB, alpha: 0.08)
-            )
-
-            /// The "Input/Background/Disabled" color.
-            @available(*, deprecated)
-            static let disabled = Color(
-                light: UIColor(0x121314, alpha: 0.04), dark: UIColor(0xF6F8FB, alpha: 0.04)
             )
 
             /// The "Input/Background/Error" color.
@@ -39,7 +48,8 @@ extension Color {
 
             /// The "Input/Background/ErrorFocused" color.
             static let errorFocused = Color(
-                light: UIColor(0xFFFFFF), dark: UIColor(0xF6F8FB, alpha: 0.08)
+                // fixme(andrii-vysotskyi): use proper color when animation is fixed
+                light: UIColor(0xFCEEED), dark: UIColor(0x3D0D04)
             )
         }
 
@@ -49,12 +59,6 @@ extension Color {
             /// The "Input/Border/Default" color.
             static let `default` = Color(
                 light: .clear, dark: UIColor(0xF6F8FB, alpha: 0.16)
-            )
-
-            /// The "Input/Border/Disabled" color.
-            @available(*, deprecated)
-            static let disabled = Color(
-                light: .clear, dark: UIColor(0xF6F8FB, alpha: 0.04)
             )
 
             /// The "Input/Border/Error" color.
@@ -81,12 +85,6 @@ extension Color {
                 light: UIColor(0x707378), dark: UIColor(0xA7A9AF)
             )
 
-            /// The "Input/Placeholder/Disabled" color.
-            @available(*, deprecated)
-            static let disabled = Color(
-                light: UIColor(0xA7A9AF), dark: UIColor(0x707378)
-            )
-
             /// The "Input/Placeholder/Error" color.
             static let error = Color(
                 light: UIColor(0x707378), dark: UIColor(0xA7A9AF)
@@ -109,12 +107,6 @@ extension Color {
             /// The "Input/Text/Default" color.
             static let `default` = Color(
                 light: UIColor(0x000000), dark: UIColor(0xFFFFFF)
-            )
-
-            /// The "Input/Text/Disabled" color.
-            @available(*, deprecated)
-            static let disabled = Color(
-                light: UIColor(0xA7A9AF), dark: UIColor(0x707378)
             )
 
             /// The "Input/Text/Error" color.
