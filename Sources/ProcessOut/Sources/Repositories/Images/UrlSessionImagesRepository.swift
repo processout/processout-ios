8b4fccc76fcd3ddfc6ee587eb396d73987cd5941
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2025-02-19 11:17:03 +0100

    feat(ad-hoc): cache images (#425)
    * Cache images returned by images repository.
    * Fix dynamic image appearance in dark mode.
    * Fix task modifier action actor isolation inheritance.
    * Fix image repository actor isolation inheritance.

--- Sources/ProcessOut/Sources/Repositories/Images/UrlSessionImagesRepository.swift
+++ Sources/ProcessOut/Sources/Repositories/Images/UrlSessionImagesRepository.swift
@@ -12,27 +12,39 @@ final class UrlSessionImagesRepository: POImagesRepository {
 
     init(session: URLSession) {
         self.session = session
+        cache = .init()
     }
 
     // MARK: - ImagesRepository
 
-    func images(at urls: [URL], scale: CGFloat) async -> [URL: UIImage] {
-        await withTaskGroup(of: (URL, UIImage?).self, returning: [URL: UIImage].self) { [session] group in
-            for url in urls {
-                group.addTask {
-                    let image = try? await UIImage(data: session.data(from: url).0, scale: scale)
-                    return (url, image)
-                }
-            }
-            var images: [URL: UIImage] = [:]
-            for await (url, image) in group {
-                images[url] = image
-            }
-            return images
+    func image(resource: POImageRemoteResource, isolation: isolated (any Actor)?) async -> UIImage? {
+        if let image = cache.value(forKey: resource) {
+            return image
         }
+        async let lightImage = image(at: resource.lightUrl), darkImage = image(at: resource.darkUrl)
+        guard let image = await UIImage.dynamic(lightImage: lightImage, darkImage: darkImage) else {
+            return nil
+        }
+        cache.insert(image, forKey: resource)
+        return image
     }
 
     // MARK: - Private Properties
 
     private let session: URLSession
+    private let cache: Cache<POImageRemoteResource, UIImage>
+
+    // MARK: - Private Methods
+
+    private func image(at resourceUrl: POImageRemoteResource.ResourceUrl?) async -> UIImage? {
+        guard let resourceUrl else {
+            return nil
+        }
+        do {
+            let data = try await session.data(from: resourceUrl.raster).0
+            return UIImage(data: data, scale: resourceUrl.scale)?.rescaledToMatchDeviceScale()
+        } catch {
+            return nil
+        }
+    }
 }
