e42140b9c82c7388910100f657d108cd7bc9d80e
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-04-15 17:11:31 +0200

    Add default style

---
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/ProgressView/StepStyle/ProgressViewStyle+Step.swift
@@ -0,0 +1,71 @@
+//
+//  ProgressViewStyle+Step.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 13.04.2025.
+//
+
+import SwiftUI
+
+@available(iOS 14.0, *)
+extension ProgressViewStyle where Self == POStepProgressViewStyle {
+
+    public static var poStep: POStepProgressViewStyle {
+        POStepProgressViewStyle(
+            notStarted: .init(
+                label: .init(
+                    color: Color(red: 134 / 255, green: 134 / 255, blue: 134 / 255),
+                    typography: .body1
+                ),
+                currentValueLabel: .init(
+                    color: Color(red: 134 / 255, green: 134 / 255, blue: 134 / 255),
+                    typography: .label1
+                ),
+                icon: .init(
+                    checkmark: nil,
+                    backgroundColor: .clear,
+                    border: .init(
+                        radius: 9999, width: 1.5, color: Color(red: 0.792, green: 0.792, blue: 0.792)
+                    ),
+                    halo: nil
+                )
+            ),
+            started: .init(
+                label: .init(
+                    color: Color(red: 88 / 255, green: 90 / 255, blue: 95 / 255),
+                    typography: .body1
+                ),
+                currentValueLabel: .init(
+                    color: Color(red: 134 / 255, green: 134 / 255, blue: 134 / 255),
+                    typography: .label1
+                ),
+                icon: .init(
+                    checkmark: nil,
+                    backgroundColor: .white,
+                    border: .init(
+                        radius: 9999, width: 1.5, color: Color(red: 0.792, green: 0.792, blue: 0.792)
+                    ),
+                    halo: .init(color: Color.black.opacity(0.08), width: 6)
+                )
+            ),
+            completed: .init(
+                label: .init(
+                    color: Color(red: 76 / 255, green: 162 / 255, blue: 89 / 255),
+                    typography: .body1
+                ),
+                currentValueLabel: .init(
+                    color: Color(red: 76 / 255, green: 162 / 255, blue: 89 / 255),
+                    typography: .label1
+                ),
+                icon: .init(
+                    checkmark: .init(color: .white, width: 2),
+                    backgroundColor: Color(red: 0.298, green: 0.635, blue: 0.349),
+                    border: .init(
+                        radius: 9999, width: 1.5, color: Color(red: 0.298, green: 0.635, blue: 0.349)
+                    ),
+                    halo: nil
+                )
+            )
+        )
+    }
+}
