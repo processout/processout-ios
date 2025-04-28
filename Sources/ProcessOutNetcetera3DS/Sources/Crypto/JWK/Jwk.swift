5f2c90b69d594aace5fa2902b9aced18e507c23a
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-04-28 16:11:48 +0200

    Support JWK decoding

---
+++ Sources/ProcessOutNetcetera3DS/Sources/Crypto/JWK/Jwk.swift
@@ -0,0 +1,38 @@
+//
+//  Jwk.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 25.04.2025.
+//
+
+// swiftlint:disable identifier_name
+
+struct Jwk: Decodable {
+
+    /// Key type.
+    let kty: String
+
+    /// The "kid" (key ID).
+    let kid: String?
+
+    // MARK: - RSA
+
+    /// RSA modulus (n), base64url-encoded
+    let n: String?
+
+    /// RSA public exponent (e), base64url-encoded
+    let e: String?
+
+    /// The named curve used.
+    let crv: String?
+
+    // MARK: - EC
+
+    /// X coordinate of the EC point, base64url-encoded
+    let x: String?
+
+    ///  Y coordinate of the EC point, base64url-encoded
+    let y: String?
+}
+
+// swiftlint:enable identifier_name
