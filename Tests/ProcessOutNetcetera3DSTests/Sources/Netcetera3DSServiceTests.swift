0626015b04e8619f0ff6ffabe23b2328b8b1ed35
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-04-29 14:03:22 +0200

    Add test target

---
+++ Tests/ProcessOutNetcetera3DSTests/Sources/Netcetera3DSServiceTests.swift
@@ -0,0 +1,15 @@
+//
+//  Checkout3DSServiceTests.swift
+//  ProcessOut
+//
+//  Created by Andrii Vysotskyi on 29.04.2025.
+//
+
+import Testing
+import ProcessOutNetcetera3DS
+
+struct Netcetera3DS2ServiceTests {
+
+    @Test
+    func example() throws { }
+}
