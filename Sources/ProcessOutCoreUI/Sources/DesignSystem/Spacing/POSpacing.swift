b04bab4668e85d9601ed26e088ce023e28b4e782
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-06-11 20:45:22 +0200

    Define new spacings

--- Sources/ProcessOutCoreUI/Sources/DesignSystem/Spacing/POSpacing.swift
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/Spacing/POSpacing.swift
@@ -7,8 +7,38 @@
 
 import Foundation
 
+/// A namespace containing standard spacing constants.
 @_spi(PO)
 public enum POSpacing {
+
+    /// 0pt spacing.
+    public static let space0: CGFloat = 0
+
+    /// 1pt spacing.
+    public static let space1: CGFloat = 1
+
+    /// 2pt spacing.
+    public static let space2: CGFloat = 2
+
+    /// 4pt spacing.
+    public static let space4: CGFloat = 4
+
+    /// 6pt spacing.
+    public static let space6: CGFloat = 6
+
+    /// 8pt spacing.
+    public static let space8: CGFloat = 8
+
+    /// 12pt spacing.
+    public static let space12: CGFloat = 12
+
+    /// 16pt spacing.
+    public static let space16: CGFloat = 16
+
+    /// 20pt spacing.
+    public static let space20: CGFloat = 20
+
+    // MARK: -
 
     /// Extra extra small spacing.
     public static let extraExtraSmall: CGFloat = 2
