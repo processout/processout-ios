c03d9dd5e762c519110dcdf72709d6c123ebe312
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-07-07 17:32:56 +0200

    Update phone number country text

--- Sources/ProcessOutCoreUI/Sources/DesignSystem/PhoneNumberField/PhoneNumberField.swift
+++ Sources/ProcessOutCoreUI/Sources/DesignSystem/PhoneNumberField/PhoneNumberField.swift
@@ -24,7 +24,7 @@ public struct POPhoneNumberField: View {
         let configuration = POPhoneNumberFieldStyleConfiguration {
             POPicker(selection: $phoneNumber.territoryId) {
                 ForEach(availableTerritories ?? []) { territory in
-                    Text("\(territory.code) \(territory.displayName)")
+                    Text("\(territory.displayName) (\(territory.code))")
                 }
             } prompt: {
                 countryPrompt
