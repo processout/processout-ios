3081121a0ce96fff964b3e41b09a7a91df71915b
Andrii Vysotskyi <andrii.vysotskyi@processout.com>
2025-08-25 13:04:05 +0200

    feat(release): 4.33.2 (#479)

--- Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
+++ Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
@@ -7,8 +7,8 @@
 
 extension ProcessOut {
 
-    /// The current version of this library, value is "4.33.1".
+    /// The current version of this library, value is "4.33.2".
     public static var version: String {
-        "4.33.1"
+        "4.33.2"
     }
 }
