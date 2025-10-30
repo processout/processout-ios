c420a870a0d66445ed3eab7c4cdf30bd905317b1
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2025-10-30 17:10:50 +0100

    Update version

--- Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
+++ Sources/ProcessOut/Sources/Generated/ProcessOut+Version.swift
@@ -7,8 +7,8 @@
 
 extension ProcessOut {
 
-    /// The current version of this library, value is "4.36.0".
+    /// The current version of this library, value is "4.35.2".
     public static var version: String {
-        "4.36.0"
+        "4.35.2"
     }
 }
