89fc2e99a05d6d511365aa29737cd307102d6187
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2026-03-30 10:23:04 +0200

    Fix query items precondition

--- Sources/ProcessOut/Sources/Services/Invoices/DefaultInvoicesService.swift
+++ Sources/ProcessOut/Sources/Services/Invoices/DefaultInvoicesService.swift
@@ -146,7 +146,7 @@ final class DefaultInvoicesService: POInvoicesService {
     }
 
     private func didReceive(deepLinkEvent event: PODeepLinkReceivedEvent) -> Bool {
-        guard event.url.queryParameters?.keys.contains("po_token") ?? false else {
+        guard event.url.queryParameters?.keys.contains(where: { $0.starts(with: "processout") }) ?? false else {
             logger.debug("Deep link url \(event.url) is not supported, ignored.")
             return false
         }
