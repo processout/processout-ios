043245cf579c56a3d172348c1607ce56e227f148
Andrii Vysotskyi <Andril.Vysotskyi@checkout.com>
2026-08-10 13:02:15 +0200

    Revert vendored Netcetera SDK update

--- Vendor/ThreeDS_SDK.xcframework/ios-arm64_x86_64-simulator/ThreeDS_SDK.framework/Headers/ThreeDS_SDK.h
+++ Vendor/ThreeDS_SDK.xcframework/ios-arm64_x86_64-simulator/ThreeDS_SDK.framework/Headers/ThreeDS_SDK.h
@@ -1,4 +1,10 @@
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+#import "NCAEllipticCurveCrypto.h"
+#import "NCAEllipticCurveCrypto+hash.h"
+#import "NCASSLCredentials.h"
+#import "NSData+NCASecurity.h"
+#import "BinaryEncryptionCheck.h"
+#import "NCAConfigParameters.h"
 
 //! Project version number for ThreeDS_SDK.
 FOUNDATION_EXPORT double ThreeDS_SDKVersionNumber;
